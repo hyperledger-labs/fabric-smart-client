222e7bc440bcdddaf7e41b999eee615b92444577
Angelo De Caro <adc@zurich.ibm.com>
2021-10-08 14:08:57 +0200

    dep fix
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ tools.go
@@ -0,0 +1,13 @@
+// +build tools
+
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package tools
+
+import (
+	_ "github.com/hyperledger/fabric/common/ledger/util/leveldbhelper"
+)
