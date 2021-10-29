58c477b44d753a58a24b797f63326b3c73127440
Angelo De Caro <adc@zurich.ibm.com>
2021-10-29 15:53:42 +0200

    dep update
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- tools.go
+++ tools.go
@@ -12,4 +12,5 @@ import (
 	_ "github.com/hyperledger/fabric/common/ledger/util/leveldbhelper"
 	_ "github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb/statecouchdb"
 	_ "github.com/hyperledger/fabric/core/ledger/pvtdatastorage"
+	_ "github.com/hyperledger/fabric/orderer/common/server"
 )
