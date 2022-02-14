385054e815afda864c8805e86808606157f270ed
Angelo De Caro <adc@zurich.ibm.com>
2022-02-14 16:28:39 +0100

    FSC identity, use ecdsa with low s #226 (#227)
    * FSC identity, use ecdsa with low s #226
    
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- platform/view/services/id/ecdsa/ecdsa.go
+++ platform/view/services/id/ecdsa/ecdsa.go
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package ecdsa
 
 import (
+	"crypto"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
@@ -14,46 +15,95 @@ import (
 	"crypto/x509"
 	"encoding/asn1"
 	"encoding/pem"
+	"fmt"
 	"math/big"
 
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/driver"
-
-	"github.com/pkg/errors"
-
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
+	"github.com/pkg/errors"
 )
 
-type ecdsaSignature struct {
+// curveHalfOrders contains the precomputed curve group orders halved.
+// It is used to ensure that signature' S value is lower or equal to the
+// curve group order halved. We accept only low-S signatures.
+// They are precomputed for efficiency reasons.
+var curveHalfOrders = map[elliptic.Curve]*big.Int{
+	elliptic.P224(): new(big.Int).Rsh(elliptic.P224().Params().N, 1),
+	elliptic.P256(): new(big.Int).Rsh(elliptic.P256().Params().N, 1),
+	elliptic.P384(): new(big.Int).Rsh(elliptic.P384().Params().N, 1),
+	elliptic.P521(): new(big.Int).Rsh(elliptic.P521().Params().N, 1),
+}
+
+func GetCurveHalfOrdersAt(c elliptic.Curve) *big.Int {
+	return big.NewInt(0).Set(curveHalfOrders[c])
+}
+
+type Signature struct {
 	R, S *big.Int
 }
 
-type dsaSigner struct {
-	sk *ecdsa.PrivateKey
+// Signer implements the crypto.Signer interface for ECDSA keys.  The
+// Sign method ensures signatures are created with Low S values since Fabric
+// normalizes all signatures to Low S.
+// See https://github.com/bitcoin/bips/blob/master/bip-0146.mediawiki#low_s
+// for more detail.
+type Signer struct {
+	PrivateKey *ecdsa.PrivateKey
 }
 
-func (d dsaSigner) Sign(message []byte) ([]byte, error) {
+// Public returns the ecdsa.PublicKey associated with PrivateKey.
+func (e *Signer) Public() crypto.PublicKey {
+	return &e.PrivateKey.PublicKey
+}
+
+// Sign signs the digest and ensures that signatures use the Low S value.
+func (e *Signer) Sign(message []byte) ([]byte, error) {
 	hash := sha256.New()
 	n, err := hash.Write(message)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to write message to hash")
+	}
 	if n != len(message) {
 		return nil, errors.Errorf("hash failure")
 	}
+	digest := hash.Sum(nil)
+
+	r, s, err := ecdsa.Sign(rand.Reader, e.PrivateKey, digest)
 	if err != nil {
 		return nil, err
 	}
-	digest := hash.Sum(nil)
 
-	return d.sk.Sign(rand.Reader, digest, nil)
+	// ensure Low S signatures
+	sig := toLowS(
+		e.PrivateKey.PublicKey,
+		Signature{
+			R: r,
+			S: s,
+		},
+	)
+
+	// return marshaled signature
+	return asn1.Marshal(sig)
 }
 
-type dsaVerifier struct {
+type Verifier struct {
 	pk *ecdsa.PublicKey
 }
 
-func (d dsaVerifier) Verify(message, sigma []byte) error {
-	signature := &ecdsaSignature{}
+func (d Verifier) Verify(message, sigma []byte) error {
+	signature := &Signature{}
 	_, err := asn1.Unmarshal(sigma, signature)
 	if err != nil {
+		return errors.Wrap(err, "failed unmarshalling signature")
+	}
+
+	lowS, err := IsLowS(d.pk, signature.S)
+	if err != nil {
 		return err
+	}
+
+	if !lowS {
+		return errors.Errorf("invalid s, must be smaller than half the order [%s][%s]", signature.S, GetCurveHalfOrdersAt(d.pk.Curve))
 	}
 
 	hash := sha256.New()
@@ -85,7 +135,7 @@ func NewSigner() (view.Identity, driver.Signer, driver.Verifier, error) {
 		return nil, nil, nil, errors.Wrap(err, "failed marshalling public key")
 	}
 
-	return pkRaw, &dsaSigner{sk: sk}, &dsaVerifier{pk: &sk.PublicKey}, nil
+	return pkRaw, &Signer{PrivateKey: sk}, &Verifier{pk: &sk.PublicKey}, nil
 }
 
 func NewSignerFromPEM(raw []byte) (driver.Signer, error) {
@@ -96,7 +146,7 @@ func NewSignerFromPEM(raw []byte) (driver.Signer, error) {
 		return nil, errors.Wrap(err, "failed unmarshalling secret key")
 	}
 
-	return &dsaSigner{sk: sk.(*ecdsa.PrivateKey)}, nil
+	return &Signer{PrivateKey: sk.(*ecdsa.PrivateKey)}, nil
 }
 
 func NewIdentityFromBytes(raw []byte) (view.Identity, driver.Verifier, error) {
@@ -109,7 +159,7 @@ func NewIdentityFromBytes(raw []byte) (view.Identity, driver.Verifier, error) {
 		return nil, nil, errors.New("expected *ecdsa.PublicKey")
 	}
 
-	return raw, &dsaVerifier{pk: publicKey}, nil
+	return raw, &Verifier{pk: publicKey}, nil
 }
 
 func NewIdentityFromPEMCert(raw []byte) (view.Identity, driver.Verifier, error) {
@@ -123,5 +173,33 @@ func NewIdentityFromPEMCert(raw []byte) (view.Identity, driver.Verifier, error) 
 		return nil, nil, errors.New("expected *ecdsa.PublicKey")
 	}
 
-	return raw, &dsaVerifier{pk: publicKey}, nil
+	return raw, &Verifier{pk: publicKey}, nil
+}
+
+/**
+When using ECDSA, both (r,s) and (r, -s mod n) are valid signatures.  In order
+to protect against signature malleability attacks, Fabric normalizes all
+signatures to a canonical form where s is at most half the order of the curve.
+In order to make signatures compliant with what Fabric expects, toLowS creates
+signatures in this canonical form.
+*/
+func toLowS(key ecdsa.PublicKey, sig Signature) Signature {
+	// calculate half order of the curve
+	halfOrder := new(big.Int).Div(key.Curve.Params().N, big.NewInt(2))
+	// check if s is greater than half order of curve
+	if sig.S.Cmp(halfOrder) == 1 {
+		// Set s to N - s so that s will be less than or equal to half order
+		sig.S.Sub(key.Params().N, sig.S)
+	}
+	return sig
+}
+
+// IsLowS checks that s is a low-S
+func IsLowS(k *ecdsa.PublicKey, s *big.Int) (bool, error) {
+	halfOrder, ok := curveHalfOrders[k.Curve]
+	if !ok {
+		return false, fmt.Errorf("curve not recognized [%s]", k.Curve)
+	}
+
+	return s.Cmp(halfOrder) != 1, nil
 }
