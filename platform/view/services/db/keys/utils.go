477e451911133abdc8e5a15444d74e8c8dd41591
Angelo De Caro <adc@zurich.ibm.com>
2022-03-03 15:43:26 +0100

    larger keys in rw sets (#242)
    * no max lenght limit
    
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- platform/view/services/db/keys/utils.go
+++ platform/view/services/db/keys/utils.go
@@ -14,8 +14,8 @@ import (
 )
 
 var (
-	nsRegexp  = regexp.MustCompile("^[a-zA-Z0-9._-]{1,20}$")
-	keyRegexp = regexp.MustCompile("^[a-zA-Z0-9._\u0000=" + string(utf8.MaxRune) + "+/-]{1,200}$")
+	nsRegexp  = regexp.MustCompile("^[a-zA-Z0-9._-]{1,50}$")
+	keyRegexp = regexp.MustCompile("^[a-zA-Z0-9._\u0000=" + string(utf8.MaxRune) + "+/-]{1,}$")
 )
 
 const NamespaceSeparator = "\u0000"
@@ -24,6 +24,7 @@ func ValidateKey(key string) error {
 	if !keyRegexp.MatchString(key) {
 		return errors.Errorf("key '%s' is invalid", key)
 	}
+	// TODO: should we enforce a length limit?
 
 	return nil
 }
