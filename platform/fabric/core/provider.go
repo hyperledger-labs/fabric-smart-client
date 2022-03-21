d8643ab3914e58712a298bc622eb67ece3d8f662
Hagar Meir <hagar.meir@ibm.com>
2022-03-21 14:14:02 +0200

    fix config MSPCacheSize
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- platform/fabric/core/provider.go
+++ platform/fabric/core/provider.go
@@ -154,7 +154,7 @@ func (p *fnsProvider) newFNS(network string) (driver.FabricNetworkService, error
 		sigService,
 		view.GetEndpointService(p.sp),
 		view.GetIdentityProvider(p.sp).DefaultIdentity(),
-		config.MSPCacheSize(500),
+		c.MSPCacheSize(500),
 	)
 	if err := mspService.Load(); err != nil {
 		return nil, errors.Wrap(err, "failed loading local msp service")
