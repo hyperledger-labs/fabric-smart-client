b0f9ea329923391efd6fccfe2e2546e07a84afec
Hagar Meir <hagar.meir@ibm.com>
2022-03-21 13:46:39 +0200

    undefined cache size
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- platform/fabric/core/provider.go
+++ platform/fabric/core/provider.go
@@ -8,11 +8,8 @@ package core
 
 import (
 	"context"
-	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/views"
 	"reflect"
 	"sync"
-
-	"github.com/pkg/errors"
 
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/config"
@@ -20,9 +17,11 @@ import (
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/finality"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/id"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/msp"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/views"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/driver"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/flogging"
+	"github.com/pkg/errors"
 )
 
 var (
@@ -155,7 +154,7 @@ func (p *fnsProvider) newFNS(network string) (driver.FabricNetworkService, error
 		sigService,
 		view.GetEndpointService(p.sp),
 		view.GetIdentityProvider(p.sp).DefaultIdentity(),
-		config.MSPCacheSize(500),
+		c.MSPCacheSize(500),
 	)
 	if err := mspService.Load(); err != nil {
 		return nil, errors.Wrap(err, "failed loading local msp service")
