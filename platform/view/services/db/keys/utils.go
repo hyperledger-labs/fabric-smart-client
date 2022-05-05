4bd3bc4fea278526940acf4928d4ded9e5192e4c
Saoussen MAROUANI <saoussen.marouani@ibm.com>
2022-05-05 11:38:52 +0200

    increase namespace length limit in rw sets #273
    Signed-off-by: Saoussen MAROUANI <saoussen.marouani@ibm.com>

--- platform/view/services/db/keys/utils.go
+++ platform/view/services/db/keys/utils.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	nsRegexp  = regexp.MustCompile("^[a-zA-Z0-9._-]{1,50}$")
+	nsRegexp  = regexp.MustCompile("^[a-zA-Z0-9._-]{1,64}$")
 	keyRegexp = regexp.MustCompile("^[a-zA-Z0-9._~\u0000=" + string(utf8.MaxRune) + "+/-]{1,}$")
 )
 
