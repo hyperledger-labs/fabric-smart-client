c3cc9fa2fee390dbc14246a72cfc06b9887b60c0
Angelo De Caro <adc@zurich.ibm.com>
2022-09-16 14:38:51 +0200

    - fabric/core/generic/id/info_test.go#L28
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- platform/fabric/core/generic/id/info_test.go
+++ platform/fabric/core/generic/id/info_test.go
@@ -9,12 +9,12 @@ package id
 import (
 	"strings"
 	"testing"
-	"time"
+
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/kvs/mock"
 
 	idemix2 "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/msp/idemix"
 	x5092 "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/msp/x509"
 	sig2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/core/sig"
-	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/db/driver/badger"
 	_ "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/db/driver/memory"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/kvs"
 	registry2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/registry"
@@ -22,61 +22,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO replace fakeProv some autogenerate mock like we use in platform/fabric/core/generic/msp/mock/config_provider
-type fakeProv struct {
-	typ  string
-	path string
-}
-
-func (f *fakeProv) GetString(key string) string {
-	return f.typ
-}
-
-func (f *fakeProv) GetInt(key string) int {
-	return 0
-}
-
-func (f *fakeProv) GetDuration(key string) time.Duration {
-	return time.Duration(0)
-}
-
-func (f *fakeProv) GetBool(key string) bool {
-	return false
-}
-
-func (f *fakeProv) GetStringSlice(key string) []string {
-	return nil
-}
-
-func (f *fakeProv) IsSet(key string) bool {
-	return false
-}
-
-func (f *fakeProv) UnmarshalKey(key string, rawVal interface{}) error {
-	*(rawVal.(*badger.Opts)) = badger.Opts{
-		Path: f.path,
-	}
-
-	return nil
-}
-
-func (f *fakeProv) ConfigFileUsed() string {
-	return ""
-}
-
-func (f *fakeProv) GetPath(key string) string {
-	return ""
-}
-
-func (f *fakeProv) TranslatePath(path string) string {
-	return ""
-}
-
 func TestInfoIdemix(t *testing.T) {
 	registry := registry2.New()
-	registry.RegisterService(&fakeProv{typ: "memory"})
 
-	kvss, err := kvs.New(registry, "memory", "")
+	kvss, err := kvs.NewWithConfig(registry, "memory", "", &mock.ConfigProvider{})
 	assert.NoError(t, err)
 	assert.NoError(t, registry.RegisterService(kvss))
 	sigService := sig2.NewSignService(registry, nil, kvss)
