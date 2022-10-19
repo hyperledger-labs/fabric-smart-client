53787ee93b681b9c1414497cfa245fbd738e6fb2
Angelo De Caro <adc@zurich.ibm.com>
2022-10-19 11:19:06 +0200

    fixup! orion commit pipeline update
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- integration/nwo/orion/platform.go
+++ integration/nwo/orion/platform.go
@@ -209,6 +209,16 @@ func (p *Platform) Cleanup() {
 	Expect(err).NotTo(HaveOccurred())
 }
 
+func (p *Platform) DeleteVault(id string) {
+	fscTopology := p.Context.TopologyByName("fsc").(*fsc.Topology)
+	for _, node := range fscTopology.Nodes {
+		if node.Name == id {
+			Expect(os.RemoveAll(p.FSCNodeVaultDir(node))).ToNot(HaveOccurred())
+		}
+	}
+	Expect(false).To(BeTrue(), "cannot find node [%d]", id)
+}
+
 func (p *Platform) replaceForDocker(origin string) string {
 	return strings.Replace(origin, p.rootDir(), "/etc/orion-server", 1)
 }
