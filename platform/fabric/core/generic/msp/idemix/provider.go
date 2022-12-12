687261001130041a850a874f2f89d1fad42622bf
sapthasurendran <saptha.surendran@ibm.com>
2022-12-12 15:00:27 +0530

    initial commit
    Signed-off-by: sapthasurendran <saptha.surendran@ibm.com>

--- platform/fabric/core/generic/msp/idemix/provider.go
+++ platform/fabric/core/generic/msp/idemix/provider.go
@@ -316,6 +316,8 @@ func (p *provider) Identity(opts *driver2.IdentityOptions) (view.Identity, []byt
 	if err := GetSignerService(p.sp).RegisterSigner(raw, sID, sID); err != nil {
 		return nil, nil, err
 	}
+//get revocationhandle from idemix
+//add attribute
 
 	var infoRaw []byte
 	switch sigType {
@@ -330,6 +332,7 @@ func (p *provider) Identity(opts *driver2.IdentityOptions) (view.Identity, []byt
 				[]byte(p.conf.Signer.OrganizationalUnitIdentifier),
 				[]byte(strconv.Itoa(getIdemixRoleFromMSPRole(role))),
 				[]byte(enrollmentID),
+				[]byte(enrollmentID), //replacement for revocationhandler
 			},
 		}
 		infoRaw, err = auditInfo.Bytes()
