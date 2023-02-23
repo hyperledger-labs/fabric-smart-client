e5facc9afa9c7ffa9525111158120edb4a55e123
alexandrosfilios <109595770+alexandrosfilios@users.noreply.github.com>
2023-02-23 19:59:39 +0100

    Changes in core template (#463)
    Not including chaincode address if port is 0. Otherwise, chaincode tries to connect to the peer as a service.
    
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- integration/nwo/fabric/topology/core_template.go
+++ integration/nwo/fabric/topology/core_template.go
@@ -17,7 +17,7 @@ peer:
   address: {{ .PeerAddress Peer "Listen" }}
   addressAutoDetect: true
   listenAddress: 0.0.0.0:{{ .PeerPort Peer "Listen" }}
-  chaincodeListenAddress: {{ .PeerAddress Peer "Chaincode" }}
+  chaincodeListenAddress: {{ if gt (.PeerPort Peer "Chaincode") 0 }}{{ .PeerAddress Peer "Chaincode" }}{{ end }}
   keepalive:
     minInterval: 60s
     interval: 300s
