c9f5cd780d9d6b6634b5ab3f963866d20c1c0cb8
Angelo De Caro <adc@zurich.ibm.com>
2023-10-18 09:33:51 +0200

    fixup! fix NewNodeFromTemplate: copy everything from the template
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- integration/nwo/fsc/fsc_test.go
+++ integration/nwo/fsc/fsc_test.go
@@ -11,14 +11,15 @@ import (
 	"context"
 	"os"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-
 	context2 "github.com/hyperledger-labs/fabric-smart-client/integration/nwo/common/context"
+	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/mocks"
+	mocks2 "github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/mocks/mocks"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/node"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/testdata/foo/initiator"
 	initiator2 "github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/testdata/initiator"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/testdata/responder"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 type DummySDK struct {
@@ -38,11 +39,14 @@ func (d *DummySDK) Start(ctx context.Context) error {
 
 var _ = Describe("EndToEnd", func() {
 	Describe("generate main", func() {
+
 		It("should not fail", func() {
 			p := NewPlatform(context2.New("", 0, nil), NewTopology(), nil)
 
 			n := node.NewNode("test")
 			n.AddSDK(&DummySDK{})
+			n.AddSDK(&mocks.SDK{})
+			n.AddSDK(&mocks2.SDK{})
 			n.RegisterViewFactory("initiator", &initiator2.Factory{})
 			n.RegisterViewFactory("initiator", &initiator.Factory{})
 			n.RegisterViewFactory("responder", &responder.Factory{})
@@ -60,5 +64,34 @@ var _ = Describe("EndToEnd", func() {
 			Expect(string(buf.Bytes())).To(BeEquivalentTo(string(ExpectedMainOne)))
 			// Expect(os.WriteFile("./testdata/main/main.go.output", buf.Bytes(), 0777)).ToNot(HaveOccurred())
 		})
+
+		It("should not fail from template", func() {
+			p := NewPlatform(context2.New("", 0, nil), NewTopology(), nil)
+
+			template := node.NewNode("test")
+			template.RegisterViewFactory("initiator", &initiator2.Factory{})
+			template.RegisterViewFactory("initiator", &initiator.Factory{})
+			template.RegisterViewFactory("responder", &responder.Factory{})
+			template.RegisterViewFactory("initiator2", &initiator2.Factory{})
+			template.RegisterResponder(&responder.Responder{}, &initiator2.Initiator{})
+
+			n := node.NewNodeFromTemplate("test", template)
+			n.AddSDK(&DummySDK{})
+			n.AddSDK(&mocks.SDK{})
+			n.AddSDK(&mocks2.SDK{})
+
+			buf := bytes.NewBuffer(nil)
+			p.GenerateCmd(buf, &node.Peer{
+				Name:         "initiator",
+				Organization: "fsc",
+				Node:         n,
+			})
+
+			ExpectedMainOne, err := os.ReadFile("./testdata/main/main.go.output")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(string(buf.Bytes())).To(BeEquivalentTo(string(ExpectedMainOne)))
+			//Expect(os.WriteFile("./testdata/main/main.go.output", buf.Bytes(), 0777)).ToNot(HaveOccurred())
+		})
+
 	})
 })
