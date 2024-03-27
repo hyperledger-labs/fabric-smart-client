6b83fc74c219ddc39680b98285cc6525814a5ea1
Angelo De Caro <adc@zurich.ibm.com>
2024-03-27 15:55:33 +0100

    fixup! - introduce ErrDiscardTX - introduce validation message
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- platform/fabric/core/generic/vault/txidstore/cache.go
+++ platform/fabric/core/generic/vault/txidstore/cache.go
@@ -10,6 +10,11 @@ import (
 	fdriver "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/driver"
 )
 
+type Entry struct {
+	ValidationCode    fdriver.ValidationCode
+	ValidationMessage string
+}
+
 type cache interface {
 	Get(key string) (interface{}, bool)
 	Add(key string, value interface{})
@@ -33,22 +38,23 @@ func NewCache(backed txidStore, cache cache) *Cache {
 func (s *Cache) Get(txID string) (fdriver.ValidationCode, string, error) {
 	// first cache
 	if val, ok := s.cache.Get(txID); ok {
-		return val.(fdriver.ValidationCode), "", nil
+		entry := val.(*Entry)
+		return entry.ValidationCode, entry.ValidationMessage, nil
 	}
 	// then backed
-	vs, msg, err := s.backed.Get(txID)
+	vc, msg, err := s.backed.Get(txID)
 	if err != nil {
-		return vs, "", err
+		return vc, "", err
 	}
-	s.cache.Add(txID, vs)
-	return vs, msg, nil
+	s.cache.Add(txID, &Entry{ValidationCode: vc, ValidationMessage: msg})
+	return vc, msg, nil
 }
 
 func (s *Cache) Set(txID string, code fdriver.ValidationCode, message string) error {
 	if err := s.backed.Set(txID, code, message); err != nil {
 		return err
 	}
-	s.cache.Add(txID, code)
+	s.cache.Add(txID, &Entry{ValidationCode: code, ValidationMessage: message})
 	return nil
 }
 
