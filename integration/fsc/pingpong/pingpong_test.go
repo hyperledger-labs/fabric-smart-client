835f6e082b3409ba90d08257d6ac1a56f1605443
Alexandros Filios <alexandros.filios@ibm.com>
2024-03-28 18:57:23 +0100

    Integration tests with replicas: Ping pong
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- integration/fsc/pingpong/pingpong_test.go
+++ integration/fsc/pingpong/pingpong_test.go
@@ -8,13 +8,13 @@ package pingpong_test
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"time"
 
 	"github.com/hyperledger-labs/fabric-smart-client/integration"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/fsc/pingpong"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/fsc/pingpong/mock"
-	api2 "github.com/hyperledger-labs/fabric-smart-client/integration/nwo/api"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/client"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/common"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc"
@@ -140,123 +140,152 @@ var _ = Describe("EndToEnd", func() {
 	})
 
 	Describe("Network-based Ping pong With LibP2P", func() {
-		s := NewTestSuite(fsc.LibP2P, integration.NoReplication)
+		s := NewTestSuite(fsc.LibP2P, false, integration.NoReplication)
 		BeforeEach(s.Setup)
 		AfterEach(s.TearDown)
-		It("generate artifacts & successful pingpong", s.TestGenerateAndPingPong)
-		It("load artifact & successful pingpong", s.TestLoadAndPingPong)
-		It("load artifact & successful pingpong with stream", s.TestLoadAndPingPongStream)
-		It("load artifact & successful stream", s.TestLoadAndStream)
-		It("load artifact & successful stream with websocket", s.TestLoadAndStreamWebsocket)
+		It("generate artifacts & successful pingpong", func() { s.TestGenerateAndPingPong("initiator") })
+		It("load artifact & successful pingpong", func() { s.TestLoadAndPingPong("initiator") })
+		It("load artifact & successful pingpong with stream", func() { s.TestLoadAndPingPongStream("initiator") })
+		It("load artifact & successful stream", func() { s.TestLoadAndStream("initiator") })
+		It("load artifact & successful stream with websocket", func() { s.TestLoadAndStreamWebsocket("initiator") })
 		It("load artifact & init clients & successful pingpong", s.TestLoadInitPingPong)
 	})
 
 	Describe("Network-based Ping pong With Websockets", func() {
-		s := NewTestSuite(fsc.WebSocket, integration.NoReplication)
+		s := NewTestSuite(fsc.WebSocket, false, integration.NoReplication)
 		BeforeEach(s.Setup)
 		AfterEach(s.TearDown)
-		It("generate artifacts & successful pingpong", s.TestGenerateAndPingPong)
-		It("load artifact & successful pingpong", s.TestLoadAndPingPong)
-		It("load artifact & successful pingpong with stream", s.TestLoadAndPingPongStream)
-		It("load artifact & successful stream", s.TestLoadAndStream)
-		It("load artifact & successful stream with websocket", s.TestLoadAndStreamWebsocket)
+		It("generate artifacts & successful pingpong", func() { s.TestGenerateAndPingPong("initiator") })
+		It("load artifact & successful pingpong", func() { s.TestLoadAndPingPong("initiator") })
+		It("load artifact & successful pingpong with stream", func() { s.TestLoadAndPingPongStream("initiator") })
+		It("load artifact & successful stream", func() { s.TestLoadAndStream("initiator") })
+		It("load artifact & successful stream with websocket", func() { s.TestLoadAndStreamWebsocket("initiator") })
 		It("load artifact & init clients & successful pingpong", s.TestLoadInitPingPong)
 	})
 
+	Describe("Network-based Ping pong With Websockets and replication", func() {
+		s := NewTestSuite(fsc.WebSocket, true, &integration.ReplicationOptions{
+			ReplicationFactors: map[string]int{
+				"initiator": 3,
+			},
+		})
+		initiatorReplicas := GetFSCReplicaNames("initiator", 3)
+		BeforeEach(s.Setup)
+		AfterEach(s.TearDown)
+		It("generate artifacts & successful pingpong", func() { s.TestGenerateAndPingPong(initiatorReplicas...) })
+		It("load artifact & successful pingpong", func() { s.TestLoadAndPingPong(initiatorReplicas...) })
+		It("load artifact & successful pingpong with stream", func() { s.TestLoadAndPingPongStream(initiatorReplicas...) })
+		It("load artifact & successful stream", func() { s.TestLoadAndStream(initiatorReplicas...) })
+		It("load artifact & successful stream with websocket", func() { s.TestLoadAndStreamWebsocket(initiatorReplicas...) })
+	})
+
 	Describe("Network-based Mock Ping pong With LibP2P", func() {
-		s := NewTestSuite(fsc.LibP2P, integration.NoReplication)
+		s := NewTestSuite(fsc.LibP2P, false, integration.NoReplication)
 		BeforeEach(s.Setup)
 		AfterEach(s.TearDown)
 		It("generate artifacts & successful mock pingpong", s.TestGenerateAndMockPingPong)
 	})
 	Describe("Network-based Mock Ping pong With Websockets", func() {
-		s := NewTestSuite(fsc.WebSocket, integration.NoReplication)
+		s := NewTestSuite(fsc.WebSocket, false, integration.NoReplication)
 		BeforeEach(s.Setup)
 		AfterEach(s.TearDown)
 		It("generate artifacts & successful mock pingpong", s.TestGenerateAndMockPingPong)
 	})
 })
 
+const testdataDir = "./testdata"
+
 type TestSuite struct {
 	*integration.TestSuite
-	topologies []api2.Topology
+	commType fsc.P2PCommunicationType
+	nodeOpts *integration.ReplicationOptions
 }
 
-func NewTestSuite(commType fsc.P2PCommunicationType, nodeOpts *integration.ReplicationOptions) *TestSuite {
+func NewTestSuite(commType fsc.P2PCommunicationType, alwaysGenerate bool, nodeOpts *integration.ReplicationOptions) *TestSuite {
 	init := atomic.NewBool(false)
-	topologies := pingpong.Topology(commType, nodeOpts)
 	return &TestSuite{
 		TestSuite: integration.NewTestSuite(func() (ii *integration.Infrastructure, err error) {
-			if !init.CompareAndSwap(false, true) {
-				ii, err = integration.Generate(StartPort(), true, topologies...)
+			topologies := pingpong.Topology(commType, nodeOpts)
+			if alwaysGenerate || init.CompareAndSwap(false, true) {
+				ii, err = integration.Generate(StartPortWithGeneration(), true, topologies...)
 			} else {
-				ii, err = integration.Load(0, "./testdata", true, topologies...)
+				ii, err = integration.Load(0, testdataDir, true, topologies...)
 			}
 			ii.DeleteOnStop = false
 			return
 		}),
-		topologies: topologies,
+		commType: commType,
+		nodeOpts: nodeOpts,
 	}
 }
 
-func (s *TestSuite) TestGenerateAndPingPong() {
+func (s *TestSuite) TestGenerateAndPingPong(clients ...string) {
 	// Initiate a view and check the output
-	res, err := s.II.Client("initiator").CallView("init", nil)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+	for _, clientName := range clients {
+		res, err := s.II.Client(clientName).CallView("init", nil)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+	}
 }
 
-func (s *TestSuite) TestLoadAndPingPong() {
+func (s *TestSuite) TestLoadAndPingPong(clients ...string) {
 	// Initiate a view and check the output
-	res, err := s.II.Client("initiator").CallView("init", nil)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+	for _, clientName := range clients {
+		res, err := s.II.Client(clientName).CallView("init", nil)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+	}
 }
 
-func (s *TestSuite) TestLoadAndPingPongStream() {
+func (s *TestSuite) TestLoadAndPingPongStream(clients ...string) {
 	// Initiate a view and check the output
-	channel, err := s.II.Client("initiator").StreamCallView("init", nil)
-	Expect(err).NotTo(HaveOccurred())
+	for _, clientName := range clients {
+		channel, err := s.II.Client(clientName).StreamCallView("init", nil)
+		Expect(err).NotTo(HaveOccurred())
 
-	res, err := channel.Result()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+		res, err := channel.Result()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+	}
 }
 
-func (s *TestSuite) TestLoadAndStream() {
-	// Initiate a view and check the output
-	channel, err := s.II.Client("initiator").StreamCallView("stream", nil)
-	Expect(err).NotTo(HaveOccurred())
-	var str string
-	Expect(channel.Recv(&str)).NotTo(HaveOccurred())
-	Expect(str).To(BeEquivalentTo("hello"))
-	Expect(channel.Send("ciao")).NotTo(HaveOccurred())
+func (s *TestSuite) TestLoadAndStream(clients ...string) {
+	for _, clientName := range clients {
+		channel, err := s.II.Client(clientName).StreamCallView("stream", nil)
+		Expect(err).NotTo(HaveOccurred())
+		var str string
+		Expect(channel.Recv(&str)).NotTo(HaveOccurred())
+		Expect(str).To(BeEquivalentTo("hello"))
+		Expect(channel.Send("ciao")).NotTo(HaveOccurred())
 
-	res, err := channel.Result()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+		res, err := channel.Result()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+	}
 }
 
-func (s *TestSuite) TestLoadAndStreamWebsocket() {
+func (s *TestSuite) TestLoadAndStreamWebsocket(clients ...string) {
 	time.Sleep(7 * time.Second)
-	// Get a client for the fsc node labelled initiator
-	initiator := s.II.WebClient("initiator")
-	// Initiate a view and check the output
-	channel, err := initiator.StreamCallView("stream", nil)
-	Expect(err).NotTo(HaveOccurred())
-	var str string
-	Expect(channel.Recv(&str)).NotTo(HaveOccurred())
-	Expect(str).To(BeEquivalentTo("hello"))
-	Expect(channel.Send("ciao")).NotTo(HaveOccurred())
+	for _, clientName := range clients {
+		// Get a client for the fsc node labelled initiator
+		initiator := s.II.WebClient(clientName)
+		// Initiate a view and check the output
+		channel, err := initiator.StreamCallView("stream", nil)
+		Expect(err).NotTo(HaveOccurred())
+		var str string
+		Expect(channel.Recv(&str)).NotTo(HaveOccurred())
+		Expect(str).To(BeEquivalentTo("hello"))
+		Expect(channel.Send("ciao")).NotTo(HaveOccurred())
 
-	res, err := channel.Result()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+		res, err := channel.Result()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(common.JSONUnmarshalString(res)).To(BeEquivalentTo("OK"))
+	}
 }
 
 func (s *TestSuite) TestLoadInitPingPong() {
 	// Use another ii to create clients
-	iiClients, err := integration.Clients("./testdata", s.topologies...)
+	iiClients, err := integration.Clients(testdataDir, pingpong.Topology(s.commType, s.nodeOpts)...)
 	Expect(err).NotTo(HaveOccurred())
 
 	// Get a client for the fsc node labelled initiator
@@ -285,4 +314,12 @@ func newWebClient(confDir string) *web.Client {
 	initiator, err := web.NewClient(c)
 	Expect(err).NotTo(HaveOccurred())
 	return initiator
+}
+
+func GetFSCReplicaNames(nodeName string, replicationFactor int) []string {
+	result := make([]string, replicationFactor)
+	for i := 0; i < replicationFactor; i++ {
+		result[i] = fmt.Sprintf("fsc.%s.%d", nodeName, i)
+	}
+	return result
 }
