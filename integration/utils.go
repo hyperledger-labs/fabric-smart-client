5bd0cdf3a15ffe9c4280826b43d612d5fdee2339
Alexandros Filios <alexandros.filios@ibm.com>
2024-03-28 14:06:25 +0100

    fixup
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

---
+++ integration/utils.go
@@ -0,0 +1,72 @@
+/*
+Copyright IBM Corp All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package integration
+
+import (
+	"time"
+
+	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fabric"
+	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc"
+	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/node"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/db/driver/sql"
+	. "github.com/onsi/gomega"
+)
+
+type NodeOptions struct {
+	ReplicationFactors map[string]int
+	SQLConfigs         map[string]*sql.PostgresConfig
+}
+
+func (o *NodeOptions) For(name string) []node.Option {
+	opts := make([]node.Option, 0, 3)
+	if f := o.ReplicationFactors[name]; f > 0 {
+		opts = append(opts, fsc.WithReplicationFactor(f))
+	}
+	if sqlConfig, ok := o.SQLConfigs[name]; ok {
+		opts = append(opts, fsc.WithPostgresPersistence(sqlConfig), fabric.WithPostgresVaultPersistence(sqlConfig))
+	}
+	return opts
+}
+
+func NewTestSuite(sqlConfigs map[string]*sql.PostgresConfig, generator func() (*Infrastructure, error)) *TestSuite {
+	return &TestSuite{
+		sqlConfigs: sqlConfigs,
+		generator:  generator,
+		closeFunc:  func() {},
+	}
+}
+
+type TestSuite struct {
+	sqlConfigs map[string]*sql.PostgresConfig
+	generator  func() (*Infrastructure, error)
+
+	closeFunc func()
+	II        *Infrastructure
+}
+
+func (s *TestSuite) TearDown() {
+	s.II.Stop()
+	s.closeFunc()
+}
+
+func (s *TestSuite) Setup() {
+	logger.Warnf("setting up for: %v", s.sqlConfigs)
+	if len(s.sqlConfigs) > 0 {
+		closeFunc, err := sql.StartPostgresWithFmt(s.sqlConfigs)
+		Expect(err).NotTo(HaveOccurred())
+		s.closeFunc = closeFunc
+	}
+
+	// Create the integration ii
+	ii, err := s.generator()
+	Expect(err).NotTo(HaveOccurred())
+	s.II = ii
+	// Start the integration ii
+	ii.Start()
+	// Sleep for a while to allow the networks to be ready
+	time.Sleep(20 * time.Second)
+}
