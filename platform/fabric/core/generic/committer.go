1da29cf137c64398a15081852a9a1dc437f54e4e
Angelo De Caro <adc@zurich.ibm.com>
2024-03-28 13:29:07 +0100

    fixup! - introduce ErrDiscardTX - introduce validation message
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- platform/fabric/core/generic/committer.go
+++ platform/fabric/core/generic/committer.go
@@ -203,6 +203,11 @@ func (c *Channel) commitUnknown(txID string, block uint64, indexInBlock int, env
 	if err := c.EnvelopeService().StoreEnvelope(txID, envelopeRaw); err != nil {
 		return errors.WithMessagef(err, "failed to store unknown envelope for [%s]", txID)
 	}
+	rws, _, err := c.RWSetLoader.GetRWSetFromEvn(txID)
+	if err != nil {
+		return errors.WithMessagef(err, "failed to get rws from envelope [%s]", txID)
+	}
+	rws.Done()
 	return c.commit(txID, nil, block, indexInBlock, envelope)
 }
 
