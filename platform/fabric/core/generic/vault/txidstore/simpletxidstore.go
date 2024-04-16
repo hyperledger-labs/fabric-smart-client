873d69295c22f54ed991cc1d1ef847b6e16e3320
Alexandros Filios <alexandros.filios@ibm.com>
2024-04-16 09:11:37 +0200

    fixup! Review findings
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- platform/fabric/core/generic/vault/txidstore/simpletxidstore.go
+++ platform/fabric/core/generic/vault/txidstore/simpletxidstore.go
@@ -172,7 +172,7 @@ func (s *SimpleTXIDStore) GetLastTxID() (string, error) {
 }
 
 func (s *SimpleTXIDStore) Iterator(pos interface{}) (fdriver.TxIDIterator, error) {
-	if ppos, ok := pos.(fdriver.SeekSet); ok {
+	if ppos, ok := pos.(*fdriver.SeekSet); ok {
 		it, err := s.persistence.GetStateSetIterator(txidNamespace, ppos.TxIDs...)
 		if err != nil {
 			return nil, err
