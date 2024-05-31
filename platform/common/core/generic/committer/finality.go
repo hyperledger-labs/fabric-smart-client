fefc0728ea1aeea27b229752344828c7ad7ac9fa
Alexandros Filios <alexandros.filios@ibm.com>
2024-05-31 14:37:25 +0200

    Prefixed committer logger with network name
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- platform/common/core/generic/committer/finality.go
+++ platform/common/core/generic/committer/finality.go
@@ -27,6 +27,7 @@ const (
 type Logger interface {
 	Debugf(template string, args ...interface{})
 	Infof(template string, args ...interface{})
+	Warnf(template string, args ...interface{})
 	Errorf(template string, args ...interface{})
 }
 
