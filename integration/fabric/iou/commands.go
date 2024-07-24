8a5072ce7c3cadb546e8cca543ada0194c17b41b
Alexandros Filios <alexandros.filios@ibm.com>
2024-07-24 16:06:23 +0200

    fixup! Responder views inherit span of initiator. Span names are prefixed with the node name. Added further spans/events.
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- integration/fabric/iou/commands.go
+++ integration/fabric/iou/commands.go
@@ -83,7 +83,6 @@ func CheckLocalMetrics(ii *integration.Infrastructure, user string, viewName str
 	logger.Infof("metrics: %v", metrics)
 	logger.Infof("Received in total %f view operations for [%s] for user %s: %v", sum, viewName, user, metrics["fsc_view_operations"].GetMetric())
 	Expect(sum).NotTo(BeZero())
-	panic("done")
 }
 
 func CheckPrometheusMetrics(ii *integration.Infrastructure, viewName string) {
