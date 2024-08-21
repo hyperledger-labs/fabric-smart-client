b1cd402e66c1d3377a092f6443726e250ae1b2ac
Alexandros Filios <alexandros.filios@ibm.com>
2024-08-21 11:06:17 +0200

    Removed redundant label from metrics
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- platform/view/services/comm/host/rest/host.go
+++ platform/view/services/comm/host/rest/host.go
@@ -20,10 +20,6 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
-const (
-	contextIDLabel tracing.LabelName = "context_id"
-)
-
 var logger = flogging.MustGetLogger("view-sdk.services.comm.rest-p2p-host")
 
 type host struct {
@@ -51,7 +47,7 @@ func NewHost(nodeID host2.PeerID, listenAddress host2.PeerIPAddress, routing rou
 		routing: routing,
 		tracer: tracerProvider.Tracer("host", tracing.WithMetricsOpts(tracing.MetricsOpts{
 			Namespace:  "viewsdk",
-			LabelNames: []tracing.LabelName{contextIDLabel},
+			LabelNames: []tracing.LabelName{},
 		})),
 	}, nil
 }
@@ -66,9 +62,7 @@ func (h *host) Start(newStreamCallback func(stream host2.P2PStream)) error {
 }
 
 func (h *host) NewStream(ctx context.Context, info host2.StreamInfo) (host2.P2PStream, error) {
-	newCtx, span := h.tracer.Start(ctx, "stream_send",
-		tracing.WithAttributes(tracing.String(contextIDLabel, info.ContextID)),
-		trace.WithSpanKind(trace.SpanKindClient))
+	newCtx, span := h.tracer.Start(ctx, "stream_send", trace.WithSpanKind(trace.SpanKindClient))
 	defer span.End()
 	//if len(address) == 0 { //TODO
 	logger.Debugf("No address passed for peer [%s]. Resolving...", info.RemotePeerID)
