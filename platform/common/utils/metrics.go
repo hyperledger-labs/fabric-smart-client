30ad0babea6b5f4f932f0012c5aa858d3b96e87e
Alexandros Filios <alexandros.filios@ibm.com>
2024-08-27 10:21:11 +0200

    Added metrics
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- platform/common/utils/metrics.go
+++ platform/common/utils/metrics.go
@@ -11,9 +11,9 @@ import (
 	"time"
 )
 
-// LinearBucketRange creates a bucket set for a histogram
+// LinearBucketTimeRange creates a bucket set for a histogram
 // that has uniform intervals between the values, e.g. 0, 1, 2, 3, 4, ...
-func LinearBucketRange(start, end time.Duration, buckets int) []float64 {
+func LinearBucketTimeRange(start, end time.Duration, buckets int) []float64 {
 	bs := make([]float64, 0, buckets+1)
 	step := (end.Seconds() - start.Seconds()) / float64(buckets)
 	for v := start.Seconds(); v <= end.Seconds(); v += step {
@@ -22,11 +22,22 @@ func LinearBucketRange(start, end time.Duration, buckets int) []float64 {
 	return bs
 }
 
+// LinearBucketRange creates a bucket set for a histogram
+// that has uniform intervals between the values, e.g. 0, 1, 2, 3, 4, ...
+func LinearBucketRange(start, end int64, buckets int) []float64 {
+	bs := make([]float64, 0, buckets+1)
+	step := float64(end-start) / float64(buckets)
+	for v := float64(start); v <= float64(end); v += step {
+		bs = append(bs, v)
+	}
+	return bs
+}
+
 const precision = float64(time.Millisecond)
 
-// ExponentialBucketRange creates a bucket set for a histogram
+// ExponentialBucketTimeRange creates a bucket set for a histogram
 // that has exponentially increasing intervals between the values, e.g. 0, 0.5, 1, 2, 4, 8, ...
-func ExponentialBucketRange(start, end time.Duration, buckets int) []float64 {
+func ExponentialBucketTimeRange(start, end time.Duration, buckets int) []float64 {
 	interval := end - start
 	factor := math.Exp(math.Log(float64(interval)/precision) / float64(buckets-1))
 	bs := make([]float64, 0, buckets)
--- platform/fabric/core/generic/committer/metrics.go
+++ platform/fabric/core/generic/committer/metrics.go
@@ -20,6 +20,9 @@ type Metrics struct {
 	NotifyFinalityDuration metrics.Histogram
 	PostFinalityDuration   metrics.Histogram
 	HandlerDuration        metrics.Histogram
+	EventQueueDuration     metrics.Histogram
+	BlockCommitDuration    metrics.Histogram
+	EventQueueLength       metrics.Gauge
 
 	Commits trace.Tracer
 	Listens trace.Tracer
@@ -31,25 +34,43 @@ func NewMetrics(p trace.TracerProvider, m metrics.Provider) *Metrics {
 			Namespace: "committer",
 			Name:      "notify_status",
 			Help:      "Histogram for the duration of notifyStatus",
-			Buckets:   utils.ExponentialBucketRange(0, 10*time.Second, 10),
+			Buckets:   utils.ExponentialBucketTimeRange(0, 1*time.Second, 10),
 		}),
 		NotifyFinalityDuration: m.NewHistogram(metrics.HistogramOpts{
 			Namespace: "committer",
 			Name:      "notify_finality",
 			Help:      "Histogram for the duration of notifyFinality",
-			Buckets:   utils.ExponentialBucketRange(0, 10*time.Second, 10),
+			Buckets:   utils.ExponentialBucketTimeRange(0, 1*time.Second, 10),
 		}),
 		PostFinalityDuration: m.NewHistogram(metrics.HistogramOpts{
 			Namespace: "committer",
 			Name:      "post_finality",
 			Help:      "Histogram for the duration of postFinality",
-			Buckets:   utils.ExponentialBucketRange(0, 10*time.Second, 10),
+			Buckets:   utils.ExponentialBucketTimeRange(0, 1*time.Second, 10),
 		}),
 		HandlerDuration: m.NewHistogram(metrics.HistogramOpts{
+			Namespace:  "committer",
+			Name:       "handler",
+			Help:       "Histogram for the duration of the committer handler",
+			LabelNames: []string{"status"},
+			Buckets:    utils.ExponentialBucketTimeRange(0, 1*time.Second, 10),
+		}),
+		BlockCommitDuration: m.NewHistogram(metrics.HistogramOpts{
 			Namespace: "committer",
-			Name:      "handler",
-			Help:      "Histogram for the duration of the committer handler",
-			Buckets:   utils.ExponentialBucketRange(0, 10*time.Second, 10),
+			Name:      "block_commit",
+			Help:      "Histogram for the duration of the block commit",
+			Buckets:   utils.ExponentialBucketTimeRange(0, 5*time.Second, 15),
+		}),
+		EventQueueDuration: m.NewHistogram(metrics.HistogramOpts{
+			Namespace: "committer",
+			Name:      "event_queue",
+			Help:      "Histogram for the duration of the event queue",
+			Buckets:   utils.ExponentialBucketTimeRange(0, 1*time.Second, 10),
+		}),
+		EventQueueLength: m.NewGauge(metrics.GaugeOpts{
+			Namespace: "committer",
+			Name:      "event_queue_length",
+			Help:      "Gauge for the length of the event queue",
 		}),
 		Commits: p.Tracer("commits", tracing.WithMetricsOpts(tracing.MetricsOpts{
 			Namespace:  "fsc",
