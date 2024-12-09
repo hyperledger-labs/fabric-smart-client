08a1ef8166317d63408f7d8d52aa2847abe13c27
Angelo De Caro <adc@zurich.ibm.com>
2024-12-09 11:02:02 +0100

    remove from the view platform references to the fabric platform
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- platform/fabric/core/generic/config/ds_test.go
+++ platform/fabric/core/generic/config/ds_test.go
@@ -4,15 +4,17 @@ Copyright IBM Corp. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 
-package config
+package config_test
 
 import (
 	"strings"
 	"testing"
 
+	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/config"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/msp/x509"
 	viperutil "github.com/hyperledger-labs/fabric-smart-client/platform/view/core/config/viper"
 	"github.com/spf13/viper"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoad(t *testing.T) {
@@ -21,11 +23,20 @@ func TestLoad(t *testing.T) {
 	v.AddConfigPath("./testdata")
 	replacer := strings.NewReplacer(".", "_")
 	v.SetEnvKeyReplacer(replacer)
-	require.NoError(t, v.ReadInConfig())
+	assert.NoError(t, v.ReadInConfig())
 
 	var value interface{}
-	require.NoError(t, v.UnmarshalKey("fabric", &value))
+	assert.NoError(t, v.UnmarshalKey("fabric", &value))
 
-	var network Network
-	require.NoError(t, viperutil.EnhancedExactUnmarshal(v, "fabric.default", &network))
+	var network config.Network
+	assert.NoError(t, viperutil.EnhancedExactUnmarshal(v, "fabric.default", &network))
+
+	b, err := x509.ToBCCSPOpts(network.MSPs[0].Opts[x509.BCCSPOptField])
+	assert.NoError(t, err)
+	assert.NotNil(t, b.SW)
+	assert.Equal(t, "SHA2", b.SW.Hash)
+	assert.NotNil(t, b.PKCS11)
+	assert.Equal(t, 256, b.PKCS11.Security)
+	assert.Equal(t, "someLabel", b.PKCS11.Label)
+	assert.Equal(t, "98765432", b.PKCS11.Pin)
 }
