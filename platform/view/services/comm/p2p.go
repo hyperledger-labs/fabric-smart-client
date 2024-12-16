f76ae154e1bd0c20a863252a3435a4c40b6ab0f7
Angelo De Caro <adc@zurich.ibm.com>
2024-12-16 15:30:52 +0100

    fix race condition (#711)
    * fix race condition
    
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>
    
    * remove comment
    
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>
    
    ---------
    
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- platform/view/services/comm/p2p.go
+++ platform/view/services/comm/p2p.go
@@ -85,11 +85,13 @@ func (p *P2PNode) Stop() {
 
 	p.host.Close()
 
+	p.streamsMutex.Lock()
 	for _, streams := range p.streams {
 		for _, stream := range streams {
 			stream.close(context.Background())
 		}
 	}
+	p.streamsMutex.Unlock()
 
 	close(p.incomingMessages)
 	p.finderWg.Wait()
@@ -334,14 +336,10 @@ func (s *streamHandler) handleIncoming() {
 }
 
 func (s *streamHandler) close(context.Context) {
-	// no need to close reader and writer when closing the stream
-	//s.reader.Close()
-	//s.writer.Close()
 	if err := s.stream.Close(); err != nil {
 		logger.Errorf("error closing stream [%s]: [%s]", s.stream.Hash(), err)
 	}
 	s.node.m.ClosedStreams.Add(1)
-	// s.wg.Wait()
 }
 
 func NewDelimitedReader(r io2.Reader, maxSize int) io.ReadCloser {
