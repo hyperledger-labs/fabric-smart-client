b5babc1715aa97958b7d1e593c0b195ff43bb6d7
Alexandros Filios <alexandros.filios@ibm.com>
2025-03-26 13:30:10 +0100

    Integration test without SP
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- integration/fsc/pingpong/cmd/responder/main.go
+++ integration/fsc/pingpong/cmd/responder/main.go
@@ -10,19 +10,17 @@ import (
 	"github.com/hyperledger-labs/fabric-smart-client/integration/fsc/pingpong"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/fsc/pingpong/mock"
 	fscnode "github.com/hyperledger-labs/fabric-smart-client/node"
-	"github.com/hyperledger-labs/fabric-smart-client/platform/view"
-	registry2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/core/registry"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/core/registry"
 )
 
 func main() {
 	node := fscnode.New()
 	node.Execute(func() error {
-		registry := view.GetRegistry(node)
-		initiatorID := registry2.GetIdentifier(&pingpong.Initiator{})
-		if err := registry.RegisterResponder(&pingpong.Responder{}, initiatorID); err != nil {
+		initiatorID := registry.GetIdentifier(&pingpong.Initiator{})
+		if err := node.RegisterResponder(&pingpong.Responder{}, initiatorID); err != nil {
 			return err
 		}
-		if err := registry.RegisterResponder(&pingpong.Responder{}, &mock.Initiator{}); err != nil {
+		if err := node.RegisterResponder(&pingpong.Responder{}, &mock.Initiator{}); err != nil {
 			return err
 		}
 		return nil
--- integration/nwo/fsc/node/node_template.go
+++ integration/nwo/fsc/node/node_template.go
@@ -17,7 +17,6 @@ package main
 import (
 	fscnode "github.com/hyperledger-labs/fabric-smart-client/node"
 
-	{{ if InstallView }}viewregistry "github.com/hyperledger-labs/fabric-smart-client/platform/view"{{ end }}
 	{{- range .Imports }}
 	{{ Alias . }} "{{ . }}"{{ end }}
 )
@@ -29,13 +28,12 @@ func main() {
 	{{ end }}
 	n.Execute(func() error {
 		{{- if InstallView }}
-		registry := viewregistry.GetRegistry(n)
 		{{- range .Factories }}
-		if err := registry.RegisterFactory("{{ .Id }}", {{ .Type }}); err != nil {
+		if err := n.RegisterFactory("{{ .Id }}", {{ .Type }}); err != nil {
 			return err
 		}{{ end }}
 		{{- range .Responders }}
-		registry.RegisterResponder({{ .Responder }}, {{ .Initiator }}){{ end }}
+		n.RegisterResponder({{ .Responder }}, {{ .Initiator }}){{ end }}
 		{{ end }}
 		return nil
 	})
