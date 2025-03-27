8f71ddd61a8fe02381be70a4572189d9aa11e0ff
Alexandros Filios <alexandros.filios@ibm.com>
2025-03-27 16:40:39 +0100

    fixup! Unit test for websocket comm service
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- platform/view/services/comm/comm_test.go
+++ platform/view/services/comm/comm_test.go
@@ -16,54 +16,89 @@ import (
 
 	"github.com/hashicorp/consul/sdk/freeport"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/common/utils"
+	endpoint2 "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/endpoint"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/msp/x509"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/core/endpoint"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/comm/host"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/comm/host/libp2p"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/comm/host/rest"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/comm/host/rest/routing"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/comm/host/rest/websocket"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/metrics/disabled"
+	view2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
 	. "github.com/onsi/gomega"
 	"go.opentelemetry.io/otel/trace/noop"
 )
 
+type node struct {
+	commService *Service
+	address     string
+	pkID        view2.Identity
+}
+
 func TestWebsocketSession(t *testing.T) {
 	RegisterTestingT(t)
 
-	addresses := map[string]string{
-		"alice": freeAddress(),
-		"bob":   freeAddress(),
+	aliceConfig, bobConfig := getConfig("initiator"), getConfig("responder")
+
+	router := &routing.StaticIDRouter{
+		"alice": []host.PeerIPAddress{rest.ConvertAddress(aliceConfig.ListenAddress)},
+		"bob":   []host.PeerIPAddress{rest.ConvertAddress(bobConfig.ListenAddress)},
 	}
-	routing := &routing.StaticIDRouter{
-		"alice": []host.PeerIPAddress{rest.ConvertAddress(addresses["alice"])},
-		"bob":   []host.PeerIPAddress{rest.ConvertAddress(addresses["bob"])},
-	}
-	rootPath, ok := os.LookupEnv("GOPATH")
-	Expect(ok).To(BeFalse(), "GOPATH is not set")
-	projectPath := path.Join(rootPath, "src", "github.com", "hyperledger-labs", "fabric-smart-client")
-	testDataPath := path.Join(projectPath, "integration", "fsc", "pingpong", "testdata", "fsc", "crypto", "peerOrganizations", "fsc.example.com", "peers")
-	aliceConfig := &Config{
-		ListenAddress: addresses["alice"],
-		KeyFile:       path.Join(testDataPath, "initiator.fsc.example.com", "msp", "keystore", "priv_sk"),
-		CertFile:      path.Join(testDataPath, "initiator.fsc.example.com", "msp", "signcerts", "initiator.fsc.example.com-cert.pem"),
-	}
-	bobConfig := &Config{
-		ListenAddress: addresses["bob"],
-		KeyFile:       path.Join(testDataPath, "responder.fsc.example.com", "msp", "keystore", "priv_sk"),
-		CertFile:      path.Join(testDataPath, "responder.fsc.example.com", "msp", "signcerts", "responder.fsc.example.com-cert.pem"),
-	}
-	alice := newWebsocketCommService(routing, aliceConfig)
+	alice := newWebsocketCommService(router, aliceConfig)
 	alice.Start(context.Background())
-	bob := newWebsocketCommService(routing, bobConfig)
+	bob := newWebsocketCommService(router, bobConfig)
 	bob.Start(context.Background())
 
+	aliceNode := node{
+		commService: alice,
+		address:     aliceConfig.ListenAddress,
+		pkID:        []byte("alice"),
+	}
+	bobNode := node{
+		commService: bob,
+		address:     bobConfig.ListenAddress,
+		pkID:        []byte("bob"),
+	}
+
+	testExchange(aliceNode, bobNode)
+}
+
+func TestLibp2pSession(t *testing.T) {
+	RegisterTestingT(t)
+
+	aliceConfig, bobConfig := getConfig("initiator"), getConfig("responder")
+	bobConfig.BootstrapNode = "alice"
+
+	alice, alicePkID := newLibP2PCommService(aliceConfig, nil)
+	alice.Start(context.Background())
+	bob, bobPkID := newLibP2PCommService(bobConfig, &bootstrapNodeResolver{nodeID: alicePkID, nodeAddress: aliceConfig.ListenAddress})
+	bob.Start(context.Background())
+
+	aliceNode := node{
+		commService: alice,
+		address:     aliceConfig.ListenAddress,
+		pkID:        alicePkID,
+	}
+	bobNode := node{
+		commService: bob,
+		address:     bobConfig.ListenAddress,
+		pkID:        bobPkID,
+	}
+
+	testExchange(aliceNode, bobNode)
+}
+
+func testExchange(aliceNode, bobNode node) {
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 
-	aliceSession, err := alice.NewSession("", "", rest.ConvertAddress(addresses["bob"]), []byte("bob"))
+	aliceSession, err := aliceNode.commService.NewSession("", "", rest.ConvertAddress(bobNode.address), bobNode.pkID)
 	Expect(err).ToNot(HaveOccurred())
-	Expect(aliceSession.Info().Endpoint).To(Equal(rest.ConvertAddress(addresses["bob"])))
-	Expect(aliceSession.Info().EndpointPKID).To(Equal([]byte("bob")))
-	bobMasterSession, err := bob.MasterSession()
+	Expect(aliceSession.Info().Endpoint).To(Equal(rest.ConvertAddress(bobNode.address)))
+	Expect(aliceSession.Info().EndpointPKID).To(Equal(bobNode.pkID.Bytes()))
+	bobMasterSession, err := bobNode.commService.MasterSession()
 	Expect(err).ToNot(HaveOccurred())
 	go func() {
 		defer wg.Done()
@@ -85,7 +120,7 @@ func TestWebsocketSession(t *testing.T) {
 		Expect(response).ToNot(BeNil())
 		Expect(response.Payload).To(Equal([]byte("msg2")))
 
-		bobSession, err := bob.NewSessionWithID(response.SessionID, "", response.FromEndpoint, response.FromPKID, nil, nil)
+		bobSession, err := bobNode.commService.NewSessionWithID(response.SessionID, "", response.FromEndpoint, response.FromPKID, nil, nil)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(bobSession.Send([]byte("msg3"))).To(Succeed())
 	}()
@@ -107,4 +142,41 @@ func newWebsocketCommService(addresses *routing.StaticIDRouter, config *Config) 
 	hostProvider := rest.NewEndpointBasedProvider(pkiExtractor, discovery, noop.NewTracerProvider(), websocket.NewMultiplexedProvider(noop.NewTracerProvider(), &disabled.Provider{}))
 
 	return newService(hostProvider, nil, config, noop.NewTracerProvider(), &disabled.Provider{})
+}
+
+func newLibP2PCommService(config *Config, resolver EndpointService) (*Service, view2.Identity) {
+	pkiExtractor := endpoint.NewPKIExtractor()
+	utils.Must(pkiExtractor.AddPublicKeyExtractor(&PKExtractor{}))
+	utils.Must(pkiExtractor.AddPublicKeyExtractor(endpoint2.PublicKeyExtractor{}))
+	pkiExtractor.SetPublicKeyIDSynthesizer(&libp2p.PKIDSynthesizer{})
+
+	hostProvider := libp2p.NewHostGeneratorProvider(&disabled.Provider{})
+
+	pkID := pkiExtractor.ExtractPKI(utils.MustGet(x509.Serialize("", config.CertFile)))
+
+	return newService(hostProvider, resolver, config, noop.NewTracerProvider(), &disabled.Provider{}), pkID
+}
+
+type bootstrapNodeResolver struct {
+	nodeAddress string
+	nodeID      []byte
+}
+
+func (r *bootstrapNodeResolver) Resolve(view2.Identity) (view2.Identity, map[view.PortName]string, []byte, error) {
+	return nil, map[view.PortName]string{view.P2PPort: rest.ConvertAddress(r.nodeAddress)}, r.nodeID, nil
+}
+
+func (r *bootstrapNodeResolver) GetIdentity(string, []byte) (view2.Identity, error) {
+	return view2.Identity("bstpnd"), nil
+}
+func getConfig(name string) *Config {
+	rootPath, ok := os.LookupEnv("GOPATH")
+	Expect(ok).To(BeFalse(), "GOPATH is not set")
+	projectPath := path.Join(rootPath, "src", "github.com", "hyperledger-labs", "fabric-smart-client")
+	mspDir := path.Join(projectPath, "integration", "fsc", "pingpong", "testdata", "fsc", "crypto", "peerOrganizations", "fsc.example.com", "peers", fmt.Sprintf("%s.fsc.example.com", name), "msp")
+	return &Config{
+		ListenAddress: freeAddress(),
+		KeyFile:       path.Join(mspDir, "keystore", "priv_sk"),
+		CertFile:      path.Join(mspDir, "signcerts", fmt.Sprintf("%s.fsc.example.com-cert.pem", name)),
+	}
 }
