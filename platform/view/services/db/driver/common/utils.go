045f1e93ecb254cf159a51f6009e0ffb15be4e84
Alexandros Filios <alexandros.filios@ibm.com>
2025-04-18 18:33:36 +0200

    re-use db
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- platform/view/services/db/driver/common/utils.go
+++ platform/view/services/db/driver/common/utils.go
@@ -6,7 +6,10 @@ SPDX-License-Identifier: Apache-2.0
 
 package common
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
 type RWDB struct {
 	ReadDB  *sql.DB
@@ -21,4 +24,11 @@ type PersistenceConstructor[O any, V DBObject] func(O) (V, error)
 func CopyPtr[T any](t T) *T {
 	v := t
 	return &v
+}
+
+func Close(readDB, writeDB *sql.DB) error {
+	if readDB == writeDB {
+		return readDB.Close()
+	}
+	return errors.Join(readDB.Close(), writeDB.Close())
 }
