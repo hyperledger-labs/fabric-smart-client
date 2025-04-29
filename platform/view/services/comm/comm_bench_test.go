51e67ebc0a248dfd8b36fb8ceb644bb9a529895c
akram <akram@il.ibm.com>
2025-04-29 10:12:56 +0200

    fixup!
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- platform/view/services/comm/comm_bench_test.go
+++ platform/view/services/comm/comm_bench_test.go
@@ -24,122 +24,131 @@ import (
 )
 
 const (
-	numOfNodes    int = 2
+	numOfNodes    int = 5
 	numOfSessions int = 1
 	numOfMsgs     int = 1
 )
 
-type Nodes struct {
-	sender       *Service
-	senderNode   Node
-	receiverNode Node
-	receiver     *Service
+type connection struct {
+	senderNode   *Node
+	receiverNode *Node
 }
 
 type benchmarkMetrics struct {
 	latency time.Duration
 }
 
-func createWebsocketNodes() []Nodes {
+func createWebsocketConnections() []*connection {
 
-	var nodes []Nodes
+	router := routing.StaticIDRouter{}
+	var configs []*Config
+	var nodeIds []string
 
-	for senderNodeNum := 0; senderNodeNum < numOfNodes; senderNodeNum++ {
+	for nodeNum := 0; nodeNum < numOfNodes; nodeNum++ {
+		nodeId := fmt.Sprintf("Node_%d", nodeNum)
+		nodeIds = append(nodeIds, nodeId)
+		config := GetConfig("initiator")
 
-		senderConfig := GetConfig("initiator")
+		logger.Infof("Creating node=%s, address=%s", nodeId, config.ListenAddress)
 
-		var receiverConfigs []*Config
-		var receivesIPAddress []host.PeerIPAddress
-		for receivedNodeNum := 0; receivedNodeNum < numOfNodes; receivedNodeNum++ {
-			if senderNodeNum == receivedNodeNum {
-				continue
-			}
-
-			receiverConfig := GetConfig("responder")
-			receivesIPAddress = append(receivesIPAddress, rest.ConvertAddress(receiverConfig.ListenAddress))
-			receiverConfigs = append(receiverConfigs, receiverConfig)
-		}
-
-		router := &routing.StaticIDRouter{
-			"sender":   []host.PeerIPAddress{rest.ConvertAddress(senderConfig.ListenAddress)},
-			"receiver": receivesIPAddress}
-
-		sender := NewWebsocketCommService(router, senderConfig.RestConfig())
-		sender.Start(context.Background())
-
-		senderNode := Node{
-			commService: sender,
-			address:     senderConfig.ListenAddress,
-			pkID:        []byte("sender"),
-		}
-
-		for _, receiverConfig := range receiverConfigs {
-			receiver := NewWebsocketCommService(router, receiverConfig.RestConfig())
-			receiver.Start(context.Background())
-			receiverNode := Node{
-				commService: receiver,
-				address:     receiverConfig.ListenAddress,
-				pkID:        []byte("receiver"),
-			}
-			nodes = append(nodes, Nodes{sender: sender, senderNode: senderNode, receiver: receiver, receiverNode: receiverNode})
-		}
+		configs = append(configs, config)
+		router[nodeId] = []host.PeerIPAddress{rest.ConvertAddress(config.ListenAddress)}
 	}
-	return nodes
-}
 
-func createLibp2pNodes() []Nodes {
+	var nodes []*Node
+	for nodeNum := 0; nodeNum < numOfNodes; nodeNum++ {
+		service := NewWebsocketCommService(&router, configs[nodeNum].RestConfig())
+		service.Start(context.Background())
+		node := Node{
+			commService: service,
+			address:     configs[nodeNum].ListenAddress,
+			pkID:        []byte(nodeIds[nodeNum]),
+		}
+		nodes = append(nodes, &node)
+	}
 
-	var nodes []Nodes
-
+	// Mesh
+	var connections []*connection
 	for senderNodeNum := 0; senderNodeNum < numOfNodes; senderNodeNum++ {
-
-		senderConfig := GetConfig("initiator")
-
-		for receivedNodeNum := 0; receivedNodeNum < numOfNodes; receivedNodeNum++ {
-			if senderNodeNum == receivedNodeNum {
+		sender := nodes[senderNodeNum]
+		for receiverNodeNum := 0; receiverNodeNum < numOfNodes; receiverNodeNum++ {
+			if senderNodeNum == receiverNodeNum {
 				continue
 			}
-
-			receiverConfig := GetConfig("responder")
-			sender, senderPkID := NewLibP2PCommService(senderConfig.Libp2pConfig(""), senderConfig.CertFile, nil)
-			sender.Start(context.Background())
-			receiver, receiverPkID := NewLibP2PCommService(receiverConfig.Libp2pConfig("sender"), receiverConfig.CertFile, &BootstrapNodeResolver{nodeID: senderPkID, nodeAddress: senderConfig.ListenAddress})
-			receiver.Start(context.Background())
-
-			senderNode := Node{
-				commService: sender,
-				address:     senderConfig.ListenAddress,
-				pkID:        senderPkID,
-			}
-
-			receiverNode := Node{
-				commService: receiver,
-				address:     receiverConfig.ListenAddress,
-				pkID:        receiverPkID,
-			}
-			nodes = append(nodes, Nodes{sender: sender, senderNode: senderNode, receiver: receiver, receiverNode: receiverNode})
+			receiver := nodes[receiverNodeNum]
+			connections = append(connections, &connection{senderNode: sender, receiverNode: receiver})
 		}
 	}
 
-	return nodes
+	// Mesh
+	// A-->B, C
+	// B-->A, C
+	// C-->B, A
+
+	// Star
+	// A, B, C
+	// A ->B, C
+	// B ->A
+	// C ->A
+
+	return connections
 }
 
-func testNodesExchange(nodes []Nodes, protocol string) {
+// func createLibp2pNodes() []Nodes {
+
+// 	var nodes []Nodes
+
+// 	for senderNodeNum := 0; senderNodeNum < numOfNodes; senderNodeNum++ {
+
+// 		senderConfig := GetConfig("initiator")
+
+// 		for receivedNodeNum := 0; receivedNodeNum < numOfNodes; receivedNodeNum++ {
+// 			if senderNodeNum == receivedNodeNum {
+// 				continue
+// 			}
+
+// 			receiverConfig := GetConfig("responder")
+// 			receiverConfig.BootstrapNode = "sender"
+// 			sender, senderPkID := NewLibP2PCommService(senderConfig, nil)
+// 			sender.Start(context.Background())
+// 			receiver, receiverPkID := NewLibP2PCommService(receiverConfig, &BootstrapNodeResolver{nodeID: senderPkID, nodeAddress: senderConfig.ListenAddress})
+// 			receiver.Start(context.Background())
+
+// 			senderNode := Node{
+// 				commService: sender,
+// 				address:     senderConfig.ListenAddress,
+// 				pkID:        senderPkID,
+// 			}
+
+// 			receiverNode := Node{
+// 				commService: receiver,
+// 				address:     receiverConfig.ListenAddress,
+// 				pkID:        receiverPkID,
+// 			}
+// 			nodes = append(nodes, Nodes{sender: sender, senderNode: senderNode, receiver: receiver, receiverNode: receiverNode})
+// 		}
+// 	}
+
+// 	return nodes
+// }
+
+func testNodesExchange(connections []*connection, protocol string) {
 	metricsMap := map[string]benchmarkMetrics{}
 
 	mainwg := sync.WaitGroup{}
 	startTime := time.Now()
 
-	mainwg.Add(len(nodes))
-	for _, exchangeNodes := range nodes {
-		go testExchangeMsgs(exchangeNodes.senderNode, exchangeNodes.receiverNode, metricsMap, &mainwg)
+	connNum := 0
+	for _, connection := range connections {
+		mainwg.Add(1)
+		go testExchangeMsgs(connNum, *connection.senderNode, *connection.receiverNode, metricsMap, &mainwg)
+		connNum++
 	}
 	mainwg.Wait()
 	endTime := time.Now()
 
-	for _, exchangeNodes := range nodes {
-		exchangeNodes.sender.Stop()
-		exchangeNodes.receiver.Stop()
+	for connNum := 0; connNum < len(connections); connNum = (connNum + numOfNodes - 1) {
+		connections[connNum].senderNode.commService.Stop()
 	}
 
 	displayMetrics(endTime.Sub(startTime), metricsMap, protocol)
@@ -147,28 +156,27 @@ func testNodesExchange(nodes []Nodes, protocol string) {
 
 func BenchmarkTestWebsocket(b *testing.B) {
 	RegisterTestingT(b)
-	testNodesExchange(createWebsocketNodes(), "Websocket")
+	testNodesExchange(createWebsocketConnections(), "Websocket")
 }
 
-func BenchmarkTestLibp2p(b *testing.B) {
-	RegisterTestingT(b)
-	testNodesExchange(createLibp2pNodes(), "LipP2P")
-}
+// func BenchmarkTestLibp2p(b *testing.B) {
+// 	RegisterTestingT(b)
+// 	testNodesExchange(createLibp2pNodes(), "LipP2P")
+// }
 
-func testExchangeMsgs(senderNode, receiverNode Node, metricsMap map[string]benchmarkMetrics, mainwg *sync.WaitGroup) {
+func testExchangeMsgs(connNum int, senderNode, receiverNode Node, metricsMap map[string]benchmarkMetrics, mainwg *sync.WaitGroup) {
 	defer mainwg.Done()
 
 	wg := sync.WaitGroup{}
-	wg.Add(2 * numOfSessions)
 
-	//sessions := make([]view.Session, 0, 2*numOfSessions+1)
 	var sessions []view.Session
 	mu := sync.Mutex{}
 
 	for sessId := 0; sessId < numOfSessions; sessId++ {
-		go senderExchangeMsgs(senderNode, receiverNode, sessId, &sessions, metricsMap, &mu, &wg)
+		wg.Add(1)
+		go senderExchangeMsgs(connNum, senderNode, receiverNode, sessId, &sessions, metricsMap, &mu, &wg)
 	}
-	go receiverExchangeMsgs(receiverNode, &sessions, &mu, &wg)
+	go receiverExchangeMsgs(connNum, receiverNode, &sessions, &mu, &wg)
 
 	logger.Infof("Waiting on execution...")
 
@@ -180,8 +188,9 @@ func testExchangeMsgs(senderNode, receiverNode Node, metricsMap map[string]bench
 	}
 }
 
-func senderExchangeMsgs(senderNode, receiverNode Node, sessId int, sessions *[]view.Session, metricsMap map[string]benchmarkMetrics, mu *sync.Mutex, wg *sync.WaitGroup) {
-	logger.Infof("---> Create new session # %d", sessId)
+func senderExchangeMsgs(connNum int, senderNode, receiverNode Node, sessId int, sessions *[]view.Session, metricsMap map[string]benchmarkMetrics, mu *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	logger.Infof("ConnNum = %d ---> Create new session # %d", connNum, sessId)
 	senderSession, err := senderNode.commService.NewSession("", "", rest.ConvertAddress(receiverNode.address), receiverNode.pkID)
 	Expect(err).ToNot(HaveOccurred())
 	mu.Lock()
@@ -189,35 +198,33 @@ func senderExchangeMsgs(senderNode, receiverNode Node, sessId int, sessions *[]v
 	mu.Unlock()
 	Expect(senderSession.Info().Endpoint).To(Equal(rest.ConvertAddress(receiverNode.address)))
 	Expect(senderSession.Info().EndpointPKID).To(Equal(receiverNode.pkID.Bytes()))
-	logger.Infof("---> Created new session # %d [%v]", sessId, senderSession.Info())
+	logger.Infof("ConnNum = %d ---> Created new session # %d [%v]", connNum, sessId, senderSession.Info())
 
 	for msgId := 0; msgId < numOfMsgs; msgId++ {
 		payload := fmt.Sprintf("request-%d-%d", sessId, msgId)
-		logger.Infof("---> Sender: sends message [%s]", payload)
+		logger.Infof("ConnNum = %d ---> Sender: sends message [%s]", connNum, payload)
 		start := time.Now()
 		Expect(senderSession.Send([]byte(payload))).To(Succeed())
-		logger.Infof("---> Sender: sent message [%s]", payload)
+		logger.Infof("ConnNum = %d ---> Sender: sent message [%s]", connNum, payload)
 		response := <-senderSession.Receive()
 		end := time.Now()
 		elapsed := end.Sub(start)
 		Expect(response).ToNot(BeNil())
 		Expect(string(response.Payload)).To(Equal(fmt.Sprintf("response-%d-%d", sessId, msgId)))
-		logger.Infof("---> Sender: received message: [%s]", string(response.Payload))
+		logger.Infof("ConnNum = %d ---> Sender: received message: [%s]", connNum, string(response.Payload))
 
-		_, ok := metricsMap[senderSession.Info().ID+strconv.Itoa(sessId)+strconv.Itoa(msgId)]
+		_, ok := metricsMap[strconv.Itoa(connNum)+senderSession.Info().ID+strconv.Itoa(sessId)+strconv.Itoa(msgId)]
 		if ok {
 			Expect(ok).To(BeFalse(), "Metrics map keys should be unique")
 		}
-
-		metricsMap[senderSession.Info().ID+strconv.Itoa(sessId)+strconv.Itoa(msgId)] = benchmarkMetrics{latency: elapsed}
+		metricsMap[strconv.Itoa(connNum)+senderSession.Info().ID+strconv.Itoa(sessId)+strconv.Itoa(msgId)] = benchmarkMetrics{latency: elapsed}
 	}
 	logger.Infof("Send EOF")
 	Expect(senderSession.Send([]byte("EOF"))).To(Succeed())
 	logger.Infof("Sent EOF")
-	defer wg.Done()
 }
 
-func receiverExchangeMsgs(receiverNode Node, sessions *[]view.Session, mu *sync.Mutex, wg *sync.WaitGroup) {
+func receiverExchangeMsgs(connNum int, receiverNode Node, sessions *[]view.Session, mu *sync.Mutex, wg *sync.WaitGroup) {
 	receiverMasterSession, err := receiverNode.commService.MasterSession()
 	Expect(err).ToNot(HaveOccurred())
 
@@ -226,10 +233,10 @@ func receiverExchangeMsgs(receiverNode Node, sessions *[]view.Session, mu *sync.
 	mu.Unlock()
 	sessionMap := map[string]struct{}{}
 
-	logger.Infof("---> Receiver: start receiving on master session")
+	logger.Infof("ConnNum = %d ---> Receiver: start receiving on master session", connNum)
 	for response := range receiverMasterSession.Receive() {
 		Expect(response).ToNot(BeNil())
-		logger.Infof("---> Receiver: received message [%s]", string(response.Payload))
+		logger.Infof("ConnNum = %d ---> Receiver: received message [%s]", connNum, string(response.Payload))
 		Expect(string(response.Payload)).To(ContainSubstring("request"))
 		elements := strings.Split(string(response.Payload), "-")
 		Expect(elements).To(HaveLen(3))
@@ -238,37 +245,38 @@ func receiverExchangeMsgs(receiverNode Node, sessions *[]view.Session, mu *sync.
 
 		_, ok := sessionMap[response.SessionID]
 		Expect(ok).To(BeFalse(), "we should not receive a second message on the master session [%s]", string(response.Payload))
-		logger.Infof("---> Receiver: open session [%d]", sessId)
+		logger.Infof("ConnNum = %d ---> Receiver: open session [%d]", connNum, sessId)
 		session, err := receiverNode.commService.NewSessionWithID(response.SessionID, "", response.FromEndpoint, response.FromPKID, nil, nil)
 		Expect(err).To(BeNil())
-		logger.Infof("---> Receiver: opened session [%d] [%v]", sessId, session.Info())
+		logger.Infof("ConnNum = %d ---> Receiver: opened session [%d] [%v]", connNum, sessId, session.Info())
 		sessionMap[response.SessionID] = struct{}{}
 		mu.Lock()
 		*sessions = append(*sessions, session)
 		mu.Unlock()
-
-		go receiverSessionExchangeMsgs(sessId, session, wg)
+		wg.Add(1)
+		go receiverSessionExchangeMsgs(connNum, sessId, session, wg)
 	}
+	logger.Infof("ConnNum = %d ---> Receiver: End receiving on master session", connNum)
 }
 
-func receiverSessionExchangeMsgs(sessId int, session view.Session, wg *sync.WaitGroup) {
+func receiverSessionExchangeMsgs(connNum int, sessId int, session view.Session, wg *sync.WaitGroup) {
 	defer wg.Done()
 	payload := fmt.Sprintf("response-%d-0", sessId)
-	logger.Infof("---> Receiver: Send message [%s]", payload)
+	logger.Infof("ConnNum = %d ---> Receiver: Send message [%s]", connNum, payload)
 	Expect(session.Send([]byte(payload))).To(Succeed())
-	logger.Infof("---> Receiver: Sent message [%s]", payload)
+	logger.Infof("ConnNum = %d ---> Receiver: Sent message [%s]", connNum, payload)
 	for response := range session.Receive() {
 		if string(response.Payload) == "EOF" {
-			logger.Infof("---> Receiver: Received EOF on [%d]", sessId)
+			logger.Infof("ConnNum = %d ---> Receiver: Received EOF on [%d]", connNum, sessId)
 			return
 		}
 		elements := strings.Split(string(response.Payload), "-")
 		Expect(elements).To(HaveLen(3))
 		sessId, msgId := utils.MustGet(strconv.Atoi(elements[1])), utils.MustGet(strconv.Atoi(elements[2]))
 		payload := fmt.Sprintf("response-%d-%d", sessId, msgId)
-		logger.Infof("---> Receiver: Send message [%s]", payload)
+		logger.Infof("ConnNum = %d ---> Receiver: Send message [%s]", connNum, payload)
 		Expect(session.Send([]byte(payload))).To(Succeed())
-		logger.Infof("---> Receiver: Sent message [%s]", payload)
+		logger.Infof("ConnNum = %d ---> Receiver: Sent message [%s]", connNum, payload)
 	}
 }
 
@@ -283,7 +291,7 @@ func displayMetrics(duration time.Duration, metricsMap map[string]benchmarkMetri
 	throughput := float64(2*numOfMsgs) / float64(duration.Seconds())
 
 	logger.Infof("============================= %s Metrics =============================\n", protocol)
-	logger.Infof("Number of nodes: %d\n", numOfNodes*2)
+	logger.Infof("Number of nodes: %d\n", numOfNodes)
 	logger.Infof("Number of sessions per node: %d\n", numOfSessions)
 	logger.Infof("Number of Msgs per session: %d\n", 2*numOfMsgs)
 	logger.Infof("Average latency: %v\n", averageLatency)
