af0c0dc4ae9cb18324378e7d5ea6d347800509e3
Hayim Shaul <hayim.shaul@ibm.com>
2025-06-30 15:56:43 +0300

    use switch instead of ifs
    Signed-off-by: Hayim Shaul <hayim.shaul@ibm.com>

--- platform/view/services/db/driver/sql/query/pagination/keyset_test.go
+++ platform/view/services/db/driver/sql/query/pagination/keyset_test.go
@@ -33,7 +33,7 @@ func setupPaginationWithLastId() *driver.PageIterator[*interface{}] {
 		Paginated(p).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(10, 200))
+	Expect(args).To(ConsistOf("10", "200"))
 
 	results := collections.NewSliceIterator([]*any{
 		common.CopyPtr[any](dbResult{StringField: "first"}),
@@ -68,7 +68,7 @@ func TestKeysetSimple(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test WHERE (col_id > $1) ORDER BY col_id ASC LIMIT $2"))
-	Expect(args).To(ConsistOf("last", 10))
+	Expect(args).To(ConsistOf("last", "10"))
 }
 
 func TestKeysetSkippingPage(t *testing.T) {
@@ -90,7 +90,7 @@ func TestKeysetSkippingPage(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(10, 220))
+	Expect(args).To(ConsistOf("10", "220"))
 }
 
 func TestKeysetGoingBack(t *testing.T) {
@@ -108,7 +108,7 @@ func TestKeysetGoingBack(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(10, 190))
+	Expect(args).To(ConsistOf("10", "190"))
 }
 
 func TestKeysetGoingNextBack(t *testing.T) {
@@ -134,7 +134,7 @@ func TestKeysetGoingNextBack(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(10, 210))
+	Expect(args).To(ConsistOf("10", "210"))
 }
 
 func TestKeysetEmptyResults(t *testing.T) {
@@ -147,7 +147,7 @@ func TestKeysetEmptyResults(t *testing.T) {
 		Paginated(p).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(10, 200))
+	Expect(args).To(ConsistOf("10", "200"))
 
 	results := collections.NewSliceIterator([]*any{})
 	page, err := pagination.NewPage[any](results, p)
@@ -163,7 +163,7 @@ func TestKeysetEmptyResults(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(10, 210))
+	Expect(args).To(ConsistOf("10", "210"))
 }
 
 func TestKeysetPartialResults(t *testing.T) {
@@ -176,7 +176,7 @@ func TestKeysetPartialResults(t *testing.T) {
 		Paginated(p).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(20, 200))
+	Expect(args).To(ConsistOf("20", "200"))
 
 	results := collections.NewSliceIterator([]*any{})
 	page, err := pagination.NewPage[any](results, p)
@@ -192,7 +192,7 @@ func TestKeysetPartialResults(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(20, 220))
+	Expect(args).To(ConsistOf("20", "220"))
 }
 
 func TestKeysetDoubleAddField(t *testing.T) {
@@ -210,7 +210,7 @@ func TestKeysetDoubleAddField(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test WHERE (col_id > $1) ORDER BY col_id ASC LIMIT $2"))
-	Expect(args).To(ConsistOf("last", 10))
+	Expect(args).To(ConsistOf("last", "10"))
 }
 
 func TestKeysetAsterixAddField(t *testing.T) {
@@ -228,7 +228,7 @@ func TestKeysetAsterixAddField(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT * FROM test WHERE (col_id > $1) ORDER BY col_id ASC LIMIT $2"))
-	Expect(args).To(ConsistOf("last", 10))
+	Expect(args).To(ConsistOf("last", "10"))
 }
 
 func TestKeysetInt(t *testing.T) {
@@ -247,7 +247,7 @@ func TestKeysetInt(t *testing.T) {
 		Paginated(p).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test ORDER BY col_id ASC LIMIT $1 OFFSET $2"))
-	Expect(args).To(ConsistOf(10, 200))
+	Expect(args).To(ConsistOf("10", "200"))
 
 	results := collections.NewSliceIterator([]*any{
 		common.CopyPtr[any](dbResult{IntField: 1}),
@@ -274,5 +274,5 @@ func TestKeysetInt(t *testing.T) {
 		Paginated(page.Pagination).
 		FormatPaginated(nil, pagination.NewDefaultInterpreter())
 	Expect(query).To(Equal("SELECT field1, col_id FROM test WHERE (col_id > $1) ORDER BY col_id ASC LIMIT $2"))
-	Expect(args).To(ConsistOf(10, 10))
+	Expect(args).To(ConsistOf(10, "10"))
 }
--- platform/view/services/db/driver/sql/query/pagination/page.go
+++ platform/view/services/db/driver/sql/query/pagination/page.go
@@ -14,41 +14,39 @@ import (
 
 // NewPage creates a new page where the id is a string
 func NewPage[V any](results collections.Iterator[*V], pagination driver.Pagination) (*driver.PageIterator[*V], error) {
-	// Check if pagination is a keyset[int, interface{}]
-	if _, ok := pagination.(*keyset[int, interface{}]); ok {
-		return NewTypedPage[int, V](results, pagination)
+	switch p := pagination.(type) {
+	case *keyset[int, interface{}]:
+		return newKeysetTypedPage[int, V](results, p)
+	case *keyset[string, interface{}]:
+		return newKeysetTypedPage[string, V](results, p)
+	case *offset:
+		return &driver.PageIterator[*V]{Items: results, Pagination: pagination}, nil
+	case *empty:
+		return &driver.PageIterator[*V]{Items: results, Pagination: pagination}, nil
+	case *none:
+		return &driver.PageIterator[*V]{Items: results, Pagination: pagination}, nil
+	default:
+		panic("Unsupported pagination type")
 	}
-
-	// Check if pagination is a keyset[string, interface{}]
-	if _, ok := pagination.(*keyset[string, interface{}]); ok {
-		return NewTypedPage[string, V](results, pagination)
-	}
-
-	// If pagination is any other keyset type, panic
-	if _, ok := pagination.(*keyset[any, any]); ok {
-		panic("Unsupported keyset type in pagination")
-	}
-
-	// If pagination is not a keyset type, return NewTypedPage[string, V]
-	return NewTypedPage[string, V](results, pagination)
 }
 
 // NewTypedPage creates a new page from the results and the previous pagination
-func NewTypedPage[I comparable, V any](results iterators.Iterator[*V], pagination driver.Pagination) (*driver.PageIterator[*V], error) {
-	if p, ok := pagination.(*keyset[I, interface{}]); ok {
-		items, err := iterators.ReadAllPointers(results)
-		if err != nil {
-			return nil, err
-		}
-		p.offsetOfLastId = p.offset + len(items)
-		if len(items) == 0 {
-			p.lastId = p.firstId
-			return &driver.PageIterator[*V]{Items: collections.NewSliceIterator[*V](items), Pagination: p}, nil
-		}
-		item := items[len(items)-1]
-		pv := p.idGetter(*item)
-		p.lastId = pv
+func newKeysetTypedPage[I comparable, V any](results iterators.Iterator[*V], pagination driver.Pagination) (*driver.PageIterator[*V], error) {
+	p, ok := pagination.(*keyset[I, interface{}])
+	if !ok {
+		return nil, nil
+	}
+	items, err := iterators.ReadAllPointers(results)
+	if err != nil {
+		return nil, err
+	}
+	p.offsetOfLastId = p.offset + len(items)
+	if len(items) == 0 {
+		p.lastId = p.firstId
 		return &driver.PageIterator[*V]{Items: collections.NewSliceIterator[*V](items), Pagination: p}, nil
 	}
-	return &driver.PageIterator[*V]{Items: results, Pagination: pagination}, nil
+	item := items[len(items)-1]
+	pv := p.idGetter(*item)
+	p.lastId = pv
+	return &driver.PageIterator[*V]{Items: collections.NewSliceIterator[*V](items), Pagination: p}, nil
 }
