f6c3bf73168901f518047a0f83167b985a2892bd
alexandrosfilios <109595770+alexandrosfilios@users.noreply.github.com>
2025-07-17 13:13:11 +0200

    Support other join types (#996)
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- platform/view/services/storage/driver/sql/query/common/table.go
+++ platform/view/services/storage/driver/sql/query/common/table.go
@@ -27,6 +27,7 @@ type Table interface {
 
 type JoinedTable interface {
 	ConditionSerializable
+	JoinAs(typ JoinType, other Table, ons ConditionSerializable) JoinedTable
 	Join(Table, ConditionSerializable) JoinedTable
 }
 
@@ -50,28 +51,57 @@ func (a aliasedTable) Field(name FieldName) Field {
 
 func (a aliasedTable) Alias() TableAlias { return a.alias }
 
-func (a aliasedTable) Join(other Table, ons ConditionSerializable) JoinedTable {
+func (a aliasedTable) JoinAs(typ JoinType, other Table, ons ConditionSerializable) JoinedTable {
 	return joinedTable{
+		types:      []JoinType{typ},
 		tables:     []Table{a, other},
 		conditions: []ConditionSerializable{ons},
 	}
 }
 
+func (a aliasedTable) Join(other Table, ons ConditionSerializable) JoinedTable {
+	return a.JoinAs(LeftInner, other, ons)
+}
+
+type JoinType int
+
+const (
+	LeftInner JoinType = iota
+	LeftOuter
+	RightInner
+	RightOuter
+	Cross
+)
+
 type joinedTable struct {
+	types      []JoinType
 	tables     []Table
 	conditions []ConditionSerializable
 }
 
-func (t joinedTable) Join(other Table, ons ConditionSerializable) JoinedTable {
+func (t joinedTable) JoinAs(typ JoinType, other Table, ons ConditionSerializable) JoinedTable {
+	t.types = append(t.types, typ)
 	t.tables = append(t.tables, other)
 	t.conditions = append(t.conditions, ons)
 	return t
 }
 
+func (t joinedTable) Join(other Table, ons ConditionSerializable) JoinedTable {
+	return t.JoinAs(LeftInner, other, ons)
+}
+
+var joinTypeMap = map[JoinType]string{
+	LeftInner:  " LEFT JOIN ",
+	LeftOuter:  " OUTER LEFT JOIN ",
+	RightInner: " INNER RIGHT JOIN ",
+	RightOuter: " OUTER RIGHT JOIN ",
+	Cross:      " CROSS JOIN ",
+}
+
 func (t joinedTable) WriteString(in CondInterpreter, sb Builder) {
 	sb.WriteConditionSerializable(t.tables[0], in)
 	for i, tt := range t.tables[1:] {
-		sb.WriteString(" LEFT JOIN ").
+		sb.WriteString(joinTypeMap[t.types[i]]).
 			WriteConditionSerializable(tt, in).
 			WriteString(" ON ").
 			WriteConditionSerializable(t.conditions[i], in)
