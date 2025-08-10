67820e0ed738de385c5d0fb28b60c95a55e2b018
Hayim.Shaul@ibm.com <hayimsha@he4cloud1.sl.cloud9.ibm.com>
2025-08-10 05:48:46 -0500

    unite multiple bind between ephemaral IDs and long-term IDs
    Signed-off-by: Hayim.Shaul@ibm.com <hayimsha@he4cloud1.sl.cloud9.ibm.com>

---
+++ platform/view/services/storage/driver/sql/postgres/binding_test.go
@@ -0,0 +1,42 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package postgres
+
+import (
+	"testing"
+
+	testing2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/storage/driver/common/testing"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/storage/driver/sql/common"
+
+	"github.com/stretchr/testify/require"
+)
+
+func newBindingStoreForTests(t *testing.T) (func(), *BindingStore) {
+	t.Log("starting postgres")
+	terminate, pgConnStr, err := StartPostgres(t, false)
+	require.NoError(t, err)
+	t.Log("postgres ready")
+
+	cp := NewConfigProvider(testing2.MockConfig(Config{
+		DataSource: pgConnStr,
+	}))
+	db, err := NewPersistenceWithOpts(cp, NewDbProvider(), "", NewBindingStore)
+	require.NoError(t, err)
+	return terminate, db
+}
+
+func TestPutBindingsMultipleEphemeralsPostgres(t *testing.T) {
+	terminate, db := newBindingStoreForTests(t)
+	defer terminate()
+	common.TestPutBindingsMultipleEphemeralsCommon(t, db.BindingStore)
+}
+
+func TestManyManyPutBindingsPostgres(t *testing.T) {
+	terminate, db := newBindingStoreForTests(t)
+	defer terminate()
+	common.TestManyManyPutBindingsCommon(t, db.BindingStore)
+}
