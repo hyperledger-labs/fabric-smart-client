2cd3a22e3f6ee177c672c2476fd185bf7840c194
Hayim.Shaul@ibm.com <hayimsha@he4cloud1.sl.cloud9.ibm.com>
2025-09-03 09:26:31 -0500

    remove obsolete code Signed-off-by: Hayim.Shaul@ibm.com <hayimsha@he4cloud1.sl.cloud9.ibm.com>

--- platform/view/services/endpoint/service_test.go
+++ platform/view/services/endpoint/service_test.go
@@ -8,11 +8,9 @@ package endpoint_test
 
 import (
 	"context"
-	"regexp"
 	"sync"
 	"testing"
 
-	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/endpoint"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/endpoint/mock"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
@@ -180,51 +178,4 @@ func TestGetIdentity(t *testing.T) {
 	require.Error(t, err)
 	assert.ErrorIs(t, err, endpoint.ErrNotFound)
 	assert.False(t, ok)
-}
-
-func TestPutBindings_MultipleEphemerals3(t *testing.T) {
-	ctx := context.Background()
-
-	// // Create mock DB and mock expectations
-	_, mock1, err := sqlmock.New()
-	// require.NoError(t, err)
-	// defer func() { _ = db.Close() }()
-
-	// // Wrap sqlmock's db into RWDB
-	// rwdb := &common3.RWDB{
-	// 	WriteDB: db,
-	// 	ReadDB:  db,
-	// }
-
-	// // Prepare table names
-	// tables := common2.TableNames{
-	// 	Binding: "bindings",
-	// }
-
-	store := &mock.BindingStore{}
-
-	// Create store using constructor
-	// store, err := NewBindingStore(rwdb, tables)
-	// require.NoError(t, err)
-
-	// Input identities
-	longTerm := view.Identity("long")
-	e1 := view.Identity("eph1")
-	e2 := view.Identity("eph2")
-
-	// Expected SQL query
-
-	expectedSQL := regexp.QuoteMeta(`SELECT long_term_id FROM bindings WHERE ephemeral_hash = $1`)
-	mock1.ExpectQuery(expectedSQL).
-		WithArgs(longTerm.UniqueID()).
-		WillReturnRows(sqlmock.NewRows([]string{"long_term_id"})) // empty rows = no results
-
-	expectedSQL = regexp.QuoteMeta(`INSERT INTO bindings (ephemeral_hash, long_term_id) VALUES ($1, $2), ($3, $4), ($5, $6) ON CONFLICT DO NOTHING;`)
-	mock1.ExpectExec(expectedSQL).
-		WithArgs(longTerm.UniqueID(), longTerm, e1.UniqueID(), longTerm, e2.UniqueID(), longTerm).
-		WillReturnResult(sqlmock.NewResult(1, 2))
-
-	err = store.PutBindings(ctx, longTerm, e1, e2)
-	require.NoError(t, err)
-	require.NoError(t, mock1.ExpectationsWereMet())
 }
