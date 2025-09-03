b2a2cb1ae8570c5cbcdf780a4f1c57207727dfa3
Hayim.Shaul@ibm.com <hayimsha@he4cloud1.sl.cloud9.ibm.com>
2025-09-03 08:58:54 -0500

    1. moved common test code to common 2. fixed postgres test
    Signed-off-by: Hayim.Shaul@ibm.com <hayimsha@he4cloud1.sl.cloud9.ibm.com>

--- platform/view/services/storage/driver/sql/sqlite/binding.go
+++ platform/view/services/storage/driver/sql/sqlite/binding.go
@@ -40,40 +40,6 @@ func newBindingStore(readDB *sql.DB, writeDB common2.WriteDB, table string) *Bin
 	}
 }
 
-// func (db *BindingStore) PutBinding(ctx context.Context, longTerm view.Identity, ephemeral view.Identity) error {
-// 	logger.DebugfContext(ctx, "put binding for pair [%s:%s]", ephemeral.UniqueID(), longTerm.UniqueID())
-// 	if lt, err := db.GetLongTerm(ctx, longTerm); err != nil {
-// 		return err
-// 	} else if lt != nil && !lt.IsNone() {
-// 		logger.DebugfContext(ctx, "replacing [%s] with long term [%s]", longTerm.UniqueID(), lt.UniqueID())
-// 		longTerm = lt
-// 	} else {
-// 		logger.DebugfContext(ctx, "Id [%s] is an unregistered long term ID", longTerm.UniqueID())
-// 	}
-// 	query := fmt.Sprintf(`
-// 		BEGIN;
-// 		INSERT INTO %s (ephemeral_hash, long_term_id)
-// 		VALUES ($1, $2)
-// 		ON CONFLICT DO NOTHING;
-// 		INSERT INTO %s (ephemeral_hash, long_term_id)
-// 		VALUES ($3, $4)
-// 		ON CONFLICT DO NOTHING;
-// 		COMMIT;
-// 		`, db.table, db.table)
-
-// 	logger.Debug(query, ephemeral.UniqueID(), longTerm.UniqueID())
-// 	_, err := db.writeDB.ExecContext(ctx, query, ephemeral.UniqueID(), longTerm, longTerm.UniqueID(), longTerm)
-// 	if err == nil {
-// 		logger.DebugfContext(ctx, "long-term and ephemeral ids registered [%s,%s]", longTerm, ephemeral)
-// 		return nil
-// 	}
-// 	if errors.Is(db.errorWrapper.WrapError(err), driver.UniqueKeyViolation) {
-// 		logger.InfofContext(ctx, "tuple [%s,%s] already in db. Skipping...", ephemeral, longTerm)
-// 		return nil
-// 	}
-// 	return errors.Wrapf(err, "failed executing query [%s]", query)
-// }
-
 func (db *BindingStore) PutBindings(ctx context.Context, longTerm view.Identity, ephemeral ...view.Identity) error {
 	if len(ephemeral) == 0 {
 		return nil
