fe6be129daffb1ccf69d2bb83966dd3d80540604
akram <akram@il.ibm.com>
2025-11-12 11:07:06 +0200

    Add more sleep to wait for the Postgres to terminate before starting it again
    Signed-off-by: akram <akram@il.ibm.com>

--- platform/view/services/storage/driver/sql/postgres/binding_test.go
+++ platform/view/services/storage/driver/sql/postgres/binding_test.go
@@ -8,6 +8,7 @@ package postgres
 
 import (
 	"testing"
+	"time"
 
 	testing2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/storage/driver/common/testing"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/storage/driver/sql/common"
@@ -16,6 +17,9 @@ import (
 )
 
 func newBindingStoreForTests(t *testing.T) (func(), *BindingStore) {
+	// When running this test together with other tests; it may happen that a container instance is still running
+	// we give this test a slow start ...
+	time.Sleep(10 * time.Second)
 	t.Log("starting postgres")
 	terminate, pgConnStr, err := StartPostgres(t, false)
 	require.NoError(t, err)
