94f2367d8847fd59b41a00e0470b269c57ae2d7d
Somesh Mal <malsomesh9@gmail.com>
2026-04-24 13:27:18 +0530

    Add utility coverage tests
    Signed-off-by: Somesh Mal <malsomesh9@gmail.com>

---
+++ platform/common/utils/assert/assert_test.go
@@ -0,0 +1,60 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package assert
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
+)
+
+func TestNoErrorDoesNotPanic(t *testing.T) {
+	t.Parallel()
+
+	require.NotPanics(t, func() {
+		NoError(nil)
+	})
+}
+
+func TestNoErrorPanicsAndReleases(t *testing.T) {
+	t.Parallel()
+
+	released := false
+	require.Panics(t, func() {
+		NoError(errors.New("boom"), func() { released = true })
+	})
+	require.True(t, released)
+}
+
+func TestValidIdentityReturnsIdentity(t *testing.T) {
+	t.Parallel()
+
+	id := view.Identity("alice")
+	require.Equal(t, id, ValidIdentity(id, nil))
+}
+
+func TestValidIdentityPanicsOnEmptyIdentity(t *testing.T) {
+	t.Parallel()
+
+	released := false
+	require.Panics(t, func() {
+		ValidIdentity(nil, nil, func() { released = true })
+	})
+	require.True(t, released)
+}
+
+func TestExtractReleasersSeparatesFunctions(t *testing.T) {
+	t.Parallel()
+
+	releaser := func() {}
+	msgs, releasers := extractReleasers("hello", 42, releaser)
+	require.Equal(t, []interface{}{"hello", 42}, msgs)
+	require.Len(t, releasers, 1)
+}
---
+++ platform/common/utils/assert/retry_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package assert
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	pkgerrors "github.com/hyperledger-labs/fabric-smart-client/pkg/utils/errors"
+)
+
+func TestRetrySucceedsImmediately(t *testing.T) {
+	t.Parallel()
+
+	calls := 0
+	err := Retry(3, 0, func() error {
+		calls++
+		return nil
+	})
+	require.NoError(t, err)
+	require.Equal(t, 1, calls)
+}
+
+func TestRetryEventuallySucceeds(t *testing.T) {
+	t.Parallel()
+
+	calls := 0
+	err := Retry(4, 0, func() error {
+		calls++
+		if calls < 3 {
+			return pkgerrors.New("not yet")
+		}
+		return nil
+	})
+	require.NoError(t, err)
+	require.Equal(t, 3, calls)
+}
+
+func TestRetryReturnsWrappedLastError(t *testing.T) {
+	t.Parallel()
+
+	sentinelErr := pkgerrors.New("still failing")
+	err := Retry(2, 0, func() error {
+		return sentinelErr
+	})
+	require.Error(t, err)
+	require.True(t, strings.Contains(err.Error(), "no luck after 2 attempts"))
+	require.True(t, strings.Contains(err.Error(), sentinelErr.Error()))
+}
+
+func TestEventuallyWithRetry(t *testing.T) {
+	t.Parallel()
+
+	calls := 0
+	EventuallyWithRetry(t, 3, time.Nanosecond, func() error {
+		calls++
+		if calls < 2 {
+			return pkgerrors.New("retry")
+		}
+		return nil
+	})
+	require.Equal(t, 2, calls)
+}
