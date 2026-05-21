3220b4e17c7768906a8a05879f22d00f225cfe12
Marcus Brandenburger <bur@zurich.ibm.com>
2026-05-21 11:56:05 +0200

    fixup! Remove koanf flag bindings
    Signed-off-by: Marcus Brandenburger <bur@zurich.ibm.com>

--- cmd/fsccli/validate/cmd.go
+++ cmd/fsccli/validate/cmd.go
@@ -51,8 +51,7 @@ func newConfigCmd() *cobra.Command {
 	}
 
 	flags := cmd.Flags()
-	flags.StringVarP(&configPath, "path", "p", "", "path to the directory containing core.yaml")
-	cmd.MarkFlagRequired("path")
+	flags.StringVarP(&configPath, "path", "p", "./", "path to the directory containing core.yaml")
 
 	return cmd
 }
