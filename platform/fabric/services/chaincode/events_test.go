75383658cf130af617ef0b4c336edc797f5c91f6
Shashank <yshashank959@gmail.com>
2026-06-09 16:13:50 +0530

    test: add chaincode unit tests for coverage
    Signed-off-by: Shashank <yshashank959@gmail.com>

---
+++ platform/fabric/services/chaincode/events_test.go
@@ -0,0 +1,112 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package chaincode_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/committer"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/services/chaincode"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/events"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/view/mock"
+)
+
+func TestEventsView(t *testing.T) {
+	t.Parallel()
+
+	mockCallback := func(event *chaincode.Event) (bool, error) {
+		return true, nil
+	}
+
+	t.Run("NewListenToEventsView", func(t *testing.T) {
+		t.Parallel()
+		v := chaincode.NewListenToEventsView("my-chaincode", mockCallback)
+		require.NotNil(t, v)
+		require.Equal(t, "my-chaincode", v.ChaincodeName)
+		require.NotNil(t, v.CallBack)
+	})
+
+	t.Run("NewListenToEventsViewWithContext", func(t *testing.T) {
+		t.Parallel()
+		ctx := t.Context()
+		v := chaincode.NewListenToEventsViewWithContext(ctx, "my-chaincode", mockCallback)
+		require.NotNil(t, v)
+		require.Equal(t, "my-chaincode", v.ChaincodeName)
+		require.NotNil(t, v.CallBack)
+		require.Equal(t, ctx, v.Ctx)
+	})
+
+	t.Run("Events errors", func(t *testing.T) {
+		t.Parallel()
+
+		v := chaincode.NewListenToEventsView("", mockCallback)
+		_, err := v.Call(nil)
+		require.ErrorContains(t, err, "no chaincode specified")
+
+		// view.Context GetService network error
+		mockCtx := &mock.Context{}
+		mockCtx.GetServiceReturns(nil, errors.New("network error"))
+
+		v2 := chaincode.NewListenToEventsView("my-chaincode", mockCallback)
+		_, err = v2.Call(mockCtx)
+		require.ErrorContains(t, err, "network error")
+	})
+
+	t.Run("Events happy path", func(t *testing.T) {
+		t.Parallel()
+
+		sub := &mockSubscriber{}
+		mockCtx, _, _ := setupMockContextWithSubscriber(sub)
+
+		eventCh := make(chan *chaincode.Event, 1)
+		mockCallback := func(event *chaincode.Event) (bool, error) {
+			eventCh <- event
+			return true, nil
+		}
+
+		v := chaincode.NewListenToEventsView("my-chaincode", mockCallback)
+		_, err := v.Call(mockCtx)
+		require.NoError(t, err)
+
+		ccEvent := &committer.ChaincodeEvent{EventName: "test-event"}
+
+		require.Eventually(t, func() bool {
+			return sub.listener != nil
+		}, 2*time.Second, 10*time.Millisecond)
+
+		sub.listener.OnReceive(&mockEvent{topic: "my-chaincode", msg: ccEvent})
+
+		select {
+		case ev := <-eventCh:
+			require.Equal(t, "test-event", ev.EventName)
+		case <-time.After(3 * time.Second):
+			t.Fatal("timeout waiting for event")
+		}
+	})
+}
+
+type mockSubscriber struct {
+	listener events.Listener
+}
+
+func (m *mockSubscriber) Subscribe(topic string, listener events.Listener) {
+	m.listener = listener
+}
+
+func (m *mockSubscriber) Unsubscribe(topic string, listener events.Listener) {}
+
+type mockEvent struct {
+	topic string
+	msg   any
+}
+
+func (m *mockEvent) Topic() string { return m.topic }
+func (m *mockEvent) Message() any  { return m.msg }
---
+++ platform/fabric/services/chaincode/query_test.go
@@ -0,0 +1,111 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package chaincode_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/services/chaincode"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/view/mock"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
+)
+
+func TestQueryView(t *testing.T) {
+	t.Parallel()
+
+	t.Run("NewQueryView", func(t *testing.T) {
+		t.Parallel()
+		v := chaincode.NewQueryView("my-chaincode", "my-func", "arg1", "arg2")
+		require.NotNil(t, v)
+		require.Equal(t, "my-chaincode", v.ChaincodeName)
+		require.Equal(t, "my-func", v.Function)
+		require.Equal(t, []any{"arg1", "arg2"}, v.Args)
+	})
+
+	t.Run("Fluent configuration methods", func(t *testing.T) {
+		t.Parallel()
+		v := chaincode.NewQueryView("my-chaincode", "my-func", "arg1")
+
+		v.WithTransientEntry("key1", "val1")
+		require.NotNil(t, v.TransientMap)
+		require.Equal(t, "val1", v.TransientMap["key1"])
+
+		v.WithNetwork("test-network")
+		require.Equal(t, "test-network", v.Network)
+
+		v.WithChannel("test-channel")
+		require.Equal(t, "test-channel", v.Channel)
+
+		v.WithMatchEndorsementPolicy()
+		require.True(t, v.MatchEndorsementPolicy)
+
+		v.WithEndorsersByMSPIDs("MSP1", "MSP2")
+		require.Equal(t, []string{"MSP1", "MSP2"}, v.EndorsersMSPIDs)
+
+		v.WithEndorsersFromMyOrg()
+		require.True(t, v.EndorsersFromMyOrg)
+
+		v.WithSignerIdentity(view.Identity("my-identity"))
+		require.Equal(t, view.Identity("my-identity"), v.InvokerIdentity)
+
+		v.WithNumRetries(3)
+		require.True(t, v.SetNumRetries)
+		require.Equal(t, uint(3), v.NumRetries)
+
+		v.WithRetrySleep(5 * time.Second)
+		require.True(t, v.SetRetrySleep)
+		require.Equal(t, 5*time.Second, v.RetrySleep)
+	})
+
+	t.Run("Query errors", func(t *testing.T) {
+		t.Parallel()
+
+		v := chaincode.NewQueryView("", "my-func", "arg1")
+		_, err := v.Query(nil)
+		require.ErrorContains(t, err, "no chaincode specified")
+
+		// view.Context GetService network error
+		mockCtx := &mock.Context{}
+		mockCtx.GetServiceReturns(nil, errors.New("network error"))
+
+		v2 := chaincode.NewQueryView("my-chaincode", "my-func", "arg1")
+		_, err = v2.Call(mockCtx)
+		require.ErrorContains(t, err, "network error")
+	})
+
+	t.Run("Query happy path", func(t *testing.T) {
+		t.Parallel()
+
+		mockCtx, mockInv, _ := setupMockContext()
+
+		v := chaincode.NewQueryView("my-chaincode", "my-func", "arg1").
+			WithTransientEntry("k1", "v1").
+			WithEndorsersByMSPIDs("Org1MSP").
+			WithEndorsersFromMyOrg().
+			WithSignerIdentity([]byte("id")).
+			WithNumRetries(2).
+			WithRetrySleep(time.Second).
+			WithMatchEndorsementPolicy()
+
+		res, err := v.Query(mockCtx)
+		require.NoError(t, err)
+		require.Equal(t, []byte("mock-result"), res)
+
+		require.Equal(t, 1, mockInv.QueryCallCount())
+		require.Equal(t, 2, mockInv.WithSignerIdentityCallCount())
+		require.Equal(t, 1, mockInv.WithTransientEntryCallCount())
+		require.Equal(t, 1, mockInv.WithEndorsersByMSPIDsCallCount())
+		require.Equal(t, 1, mockInv.WithEndorsersFromMyOrgCallCount())
+		require.Equal(t, 1, mockInv.WithNumRetriesCallCount())
+		require.Equal(t, 1, mockInv.WithRetrySleepCallCount())
+		require.Equal(t, 1, mockInv.WithMatchEndorsementPolicyCallCount())
+	})
+}
