0e574242c56ee3bd3378097914f250767a618965
Said Altury <109276519+SaidAltury-ibm@users.noreply.github.com>
2026-06-15 12:34:48 +0300

    feat(build): add make target for proto generation (#1535)
    Signed-off-by: Said Altury <Said.Altury@ibm.com>

--- platform/view/services/grpc/generate_test.go
+++
@@ -1,9 +0,0 @@
-/*
-Copyright IBM Corp. All Rights Reserved.
-
-SPDX-License-Identifier: Apache-2.0
-*/
-
-//go:generate protoc --proto_path=testdata/grpc --go_out=paths=source_relative:testpb --go-grpc_out=paths=source_relative:testpb testdata/grpc/test.proto
-
-package grpc_test
