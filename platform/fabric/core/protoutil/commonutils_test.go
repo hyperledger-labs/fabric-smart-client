e87ddcbfb8f9e4e2a68f79995cf8175b4e244f10
Siddhi Khandelwal <siddhi.200727@gmail.com>
2026-09-23 17:00:06 +0530

    Update platform/fabric/core/protoutil/commonutils_test.go
    Co-authored-by: Marcus Brandenburger <mbrandenburger@users.noreply.github.com>
    Signed-off-by: Siddhi Khandelwal <siddhi.200727@gmail.com>

--- platform/fabric/core/protoutil/commonutils_test.go
+++ platform/fabric/core/protoutil/commonutils_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 
 	cb "github.com/hyperledger/fabric-protos-go-apiv2/common"
-	"github.com/hyperledger/fabric-protos-go-apiv2/peer"
+	pb "github.com/hyperledger/fabric-protos-go-apiv2/peer"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
